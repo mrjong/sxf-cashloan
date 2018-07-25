d643b464cfb21c781988801f4fe151c7cfee98e6
张晋佩 <302784355@qq.com>
2018-07-25 16:15:12 +0800

    添加首页，修改footer组件

--- src/pages/home/home/index.js
+++ src/pages/home/home/index.js
@@ -1,20 +1,26 @@
+import sng4 from 'assets/images/carousel/banner.png';
 import React, { PureComponent } from 'react';
+import Carousels from 'components/carousel';
+import InfoCard from '../components/info_card/index.js';
 
-export default class Home extends PureComponent {
+import style from './style.scss';
+
+export default class HomePage extends PureComponent {
   constructor(props) {
     super(props);
   }
 
-  cc() {
-    this.props.history.push('/login/login')
-  }
+  handleClickBack = () => {
+    console.log('代还');
+  };
 
   render() {
     return (
-      <div>
-        <div onClick={() => this.cc()}> 首页</div>
+      <div className={style.home_page}>
+        <Carousels data={[{ src: sng4, url: '' }, { src: sng4, url: '' }, { src: sng4, url: '' }]} />
+        <InfoCard onClick={this.handleClickBack} />
+        <div className={style.tip_bottom}>怕逾期，用还到</div>
       </div>
-    )
+    );
   }
 }
-
