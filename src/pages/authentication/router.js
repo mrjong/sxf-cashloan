00f9ae04ab8f1e626a622c21a467c1d9b9f708ae
闻佳 <wen_j@suixingpay.com>
2018-07-25 11:18:34 +0800

    修改了下路由

--- src/pages/authentication/router.js
+++ src/pages/authentication/router.js
@@ -1,20 +1,16 @@
 export default [
   {
     path: '/authentication/essential_information',
-    title: {
-      title: '基本信息认证',
-      headerHide: true,
-      footerHide: true,
-    },
+    title: '基本信息认证',
+    headerHide: true,
+    footerHide: true,
     component: () => import('pages/authentication/essential_information'),
   },
   {
     path: '/authentication/real_name',
-    title: {
-      title: '实名认证',
-      headerHide: true,
-      footerHide: true,
-    },
+    title: '实名认证',
+    headerHide: true,
+    footerHide: true,
     component: () => import('pages/authentication/real_name'),
   },
 ];
