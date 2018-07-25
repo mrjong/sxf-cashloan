171800f6f083e28c81e5a6ee6a25360065494b6f
闻佳 <wen_j@suixingpay.com>
2018-07-25 10:49:37 +0800

    添加了实名认证页面

---
+++ src/pages/authentication/essential_information/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import parentComponent from 'pages/common/parentComponent';
+
+export default class essential_information extends parentComponent {
+  state = {};
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>基本信息</div>
+    );
+  }
+}
--- src/pages/router.js
+++ src/pages/router.js
@@ -1,4 +1,5 @@
 import AuthRoute from 'pages/home/router'; // 首页
 import LoginRoute from 'pages/login/router'; // 首页
+import AuthenticationRoute from 'pages/authentication/router'; // 授信项
 
-export default [...AuthRoute, ...LoginRoute];
+export default [...AuthRoute, ...LoginRoute,...AuthenticationRoute];
