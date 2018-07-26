5f396451cbd6fa82d8ff55072ac787da10f5f764
FloweringVivian <flowering_season@163.com>
2018-07-26 16:42:43 +0800

    增加校验js，单独提出一个常用校验的js

---
+++ src/utils/validator.js
@@ -0,0 +1,30 @@
+export const validator = {
+    bankCardNumber: val => /^\d{16}|\d{19}$/.test(val), // 银行卡号
+    phone: val => /^1\d{10}$/.test(val), // 手机为以1开头的11位数字
+    IDCardNumber: val => /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(val), // 身份证 中国的身份证号，一代身份证号是15位的数字，二代身份证都是18位的，最后一位校验位除了可能是数字还可能是‘X‘或‘x‘，所以有四种可能性：a.15位数字 b.18位数字 c.17位数字，第十八位是‘X‘ d.17位数字，第十八位是‘x‘
+    enLength: (val, min, max) => { // 英文字符长度校验
+        if (val.replace(/(^\s*)|(\s*$)/g, "") === "") { // /^(?!(\s+$))/
+            return false;
+        } else if (min) {
+            if (val.length < min) {
+                return false
+            } else {
+                if (max) {
+                    if (val.length > max) {
+                        return false
+                    } else {
+                        return true
+                    }
+                } else {
+                    return true
+                }
+            }
+        }
+    },
+    chLength: (val, min, max) => { // 中文字符长度校验
+        let minNum = min ? min : 1;
+        let maxNum = max ? max : 100000;
+        let chReg = new RegExp("^(([\u4e00-\u9fa5])|(\\.)|(\\·)){"+minNum+","+maxNum+"}$");
+        return chReg.test(val);
+    }
+}
