afff64379262308f6afb7b0bd79fe8c73e890161
张晋佩 <302784355@qq.com>
2018-07-28 18:40:28 +0800

    修改首页点击事件逻辑

--- src/pages/home/home/components/bank_content/index.js
+++ src/pages/home/home/components/bank_content/index.js
@@ -9,29 +9,40 @@ export default class BankContent extends React.PureComponent {
   static propTypes = {
     className: PropTypes.string,
     children: PropTypes.node,
-    showModalFun: PropTypes.func,
+    history:PropTypes.object,
     contentData: PropTypes.object,
   };
 
   static defaultProps = {
     className: '',
     children: '',
+    history: {},
     contentData: {},
-    showModalFun: () => {
-      console.log('该弹框了');
-    },
+  };
+
+  // 代还其他信用卡点击事件
+  repayForOtherBank = () => {
+    if(this.props.haselescard === 'true') {
+      console.log('跳选择授信卡页');
+      // this.props.push('');
+    } else {
+      console.log('跳魔蝎');
+      // this.props.push('');
+    }
   };
 
   render() {
-    const { className, children, showModalFun, contentData, ...restProps } = this.props;
+    const { className, children, contentData, ...restProps } = this.props;
     return (
       <div className={style.bank_content_wrap} {...restProps}>
         <BankCard contentData={contentData} {...contentData.indexData} />
         {children}
-        <button className={style.link_tip} onClick={event => _handleClick(showModalFun, event)}>
-          代还其它信用卡
-          <img className={style.link_arrow_img} src={iconArrow} alt="" />
-        </button>
+        {contentData.indexMsg === '一键还卡' ? (
+          <button className={style.link_tip} onClick={this.repayForOtherBank}>
+            代还其它信用卡
+            <img className={style.link_arrow_img} src={iconArrow} alt="" />
+          </button>
+        ) : null}
       </div>
     );
   }
