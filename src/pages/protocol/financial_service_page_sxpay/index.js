2a2bb068329aa5d1ebd4b0566ca0f67aec2dcccf
张晋佩 <302784355@qq.com>
2018-08-04 14:24:15 +0800

    添加金融服务协议

---
+++ src/pages/protocol/financial_service_page_sxpay/index.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+import styles from '../index.scss';
+export default class financial_service_page_sxpay extends PureComponent {
+    render() {
+        return (
+            <iframe
+                className={styles.container}
+                src="/disting/#/financial_service_page_sxpay"
+                name="financial_service_page_sxpay"
+                id="financial_service_page_sxpay"
+                width="100%"
+                height="100%"
+                frameBorder="0"
+            />
+        )
+    }
+}
--- src/pages/protocol/router.js
+++ src/pages/protocol/router.js
@@ -1,33 +1,42 @@
 export default [
-    {
-        path: '/protocol/privacy_agreement_page',
-        title: '随行付用户隐私协议',
-        component: () => import('pages/protocol/privacy_agreement_page'),
-    },
-    {
-        path: '/protocol/register_agreement_page',
-        title: '用户注册协议',
-        component: () => import('pages/protocol/register_agreement_page'),
-    }, {
-        path: '/protocol/loan_contract_page',
-        title: '借款合同',
-        component: () => import('pages/protocol/loan_contract_page'),
-    },
-    {
-        path: '/protocol/delegation_withhold_page',
-        title: '委托扣款协议',
-        component: () => import('pages/protocol/delegation_withhold_page'),
-    }, {
-        path: '/protocol/financial_service_page',
-        title: '金融服务协议',
-        component: () => import('pages/protocol/financial_service_page'),
-    }, {
-        path: '/protocol/shortcut_bind_card_page',
-        title: '随行付快捷绑卡支付协议',
-        component: () => import('pages/protocol/shortcut_bind_card_page'),
-    }, {
-        path: '/protocol/club_vip_service_page',
-        title: '随行付VIP俱乐部会员服务协议',
-        component: () => import('pages/protocol/club_vip_service_page'),
-    }
+  {
+    path: '/protocol/privacy_agreement_page',
+    title: '随行付用户隐私协议',
+    component: () => import('pages/protocol/privacy_agreement_page'),
+  },
+  {
+    path: '/protocol/register_agreement_page',
+    title: '用户注册协议',
+    component: () => import('pages/protocol/register_agreement_page'),
+  },
+  {
+    path: '/protocol/loan_contract_page',
+    title: '借款合同',
+    component: () => import('pages/protocol/loan_contract_page'),
+  },
+  {
+    path: '/protocol/delegation_withhold_page',
+    title: '委托扣款协议',
+    component: () => import('pages/protocol/delegation_withhold_page'),
+  },
+  {
+    path: '/protocol/financial_service_page',
+    title: '金融服务协议',
+    component: () => import('pages/protocol/financial_service_page'),
+  },
+  {
+    path: '/protocol/financial_service_page_sxpay',
+    title: '随行付金融服务协议',
+    component: () => import('pages/protocol/financial_service_page_sxpay'),
+  },
+  {
+    path: '/protocol/shortcut_bind_card_page',
+    title: '随行付快捷绑卡支付协议',
+    component: () => import('pages/protocol/shortcut_bind_card_page'),
+  },
+  {
+    path: '/protocol/club_vip_service_page',
+    title: '随行付VIP俱乐部会员服务协议',
+    component: () => import('pages/protocol/club_vip_service_page'),
+  },
 ];
