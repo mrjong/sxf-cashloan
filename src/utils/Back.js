564810ae2a014012c29a3306482e4695bfea3b22
shawn <787990963@qq.com>
2018-08-09 16:31:26 +0800

    --

--- src/utils/Back.js
+++ src/utils/Back.js
@@ -1,37 +1,37 @@
-// import { logoutAppHandler } from 'utils/common';
-// import qs from 'qs'
-// import { store } from 'utils/common'
-// if (window.history && window.history.pushState) {
-//   $(window).on('popstate', function () {
-//     var hashLocation = location.hash;
-//     var hashSplit = hashLocation.split("#!/");
-//     var hashName = hashSplit[1];
-//     if (hashName !== '') {
-//       let historyRouter = store.getHistoryRouter()
-//       var hash = window.location.hash;
-//       if (hash === '') {
-//         switch (historyRouter) {
-//           case '/home/home':
-//           case '/order/order_page':
-//           case '/mine/mine_page':
-//             logoutAppHandler()
-//             break;
-//           case '/order/repayment_succ_page':
-//             window.ReactRouterHistory.push('/home/home')
-//             break;
-//           case '/mine/credit_extension_page':
-//             const queryData = qs.parse(location.search, { ignoreQueryPrefix: true });
-//             if (queryData.isShowCommit === 'true') {
-//               window.ReactRouterHistory.push('/home/home')
-//             } else {
-//               window.ReactRouterHistory.push('/mine/mine_page')
-//             }
-//             break;
-//           default:
-//             // window.ReactRouterHistory.goBack()
-//             break
-//         }
-//       }
-//     }
-//   });
-// }
+import { logoutAppHandler } from 'utils/common';
+import qs from 'qs'
+import { store } from 'utils/common'
+if (window.history && window.history.pushState) {
+  $(window).on('popstate', function () {
+    var hashLocation = location.hash;
+    var hashSplit = hashLocation.split("#!/");
+    var hashName = hashSplit[1];
+    if (hashName !== '') {
+      let historyRouter = store.getHistoryRouter()
+      var hash = window.location.hash;
+      if (hash === '') {
+        switch (historyRouter) {
+          case '/home/home':
+          case '/order/order_page':
+          case '/mine/mine_page':
+            logoutAppHandler()
+            break;
+          case '/order/repayment_succ_page':
+            window.ReactRouterHistory.push('/home/home')
+            break;
+          case '/mine/credit_extension_page':
+            const queryData = qs.parse(location.search, { ignoreQueryPrefix: true });
+            if (queryData.isShowCommit === 'true') {
+              window.ReactRouterHistory.push('/home/home')
+            } else {
+              window.ReactRouterHistory.push('/mine/mine_page')
+            }
+            break;
+          default:
+            // window.ReactRouterHistory.goBack()
+            break
+        }
+      }
+    }
+  });
+}
--- src/utils/noRouterBack.js
+++ src/utils/noRouterBack.js
@@ -1,18 +1,18 @@
 
 module.exports = () => {
-    // let home = ['/home/home', '/order/order_page', '/mine/mine_page', '/mine/credit_extension_page']
+    let home = ['/home/home', '/order/order_page', '/mine/mine_page', '/mine/credit_extension_page', '/order/repayment_succ_page']
 
-    // if (window.history && window.history.pushState) {
-    //     if (home.includes(location.pathname)) {
-    //         $(window).on('popstate', function () {
-    //             window.history.pushState('forward', null, '#');
-    //             window.history.forward(1);
-    //         });
-    //     }
+    if (window.history && window.history.pushState) {
+        $(window).on('popstate', function () {
+            if (home.includes(location.pathname)) {
+                window.history.pushState('forward', null, '#');
+                window.history.forward(1);
+            }
+        });
+    }
 
-    // }
-    // if (home.includes(location.pathname)) {
-    //     window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
-    //     window.history.forward(1);
-    // }
+    if (home.includes(location.pathname)) {
+        window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
+        window.history.forward(1);
+    }
 }
