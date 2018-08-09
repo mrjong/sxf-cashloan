c55f637ce2af644b4466f7c0346112edaa25c2da
shawn <787990963@qq.com>
2018-08-09 16:04:56 +0800

    --

--- src/utils/noRouterBack.js
+++ src/utils/noRouterBack.js
@@ -1,11 +1,18 @@
 
 module.exports = () => {
-    if (window.history && window.history.pushState) {
-        $(window).on('popstate', function () {
-            window.history.pushState('forward', null, '#');
-            window.history.forward(1);
-        });
-    }
-    window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
-    window.history.forward(1);
+    // let home = ['/home/home', '/order/order_page', '/mine/mine_page', '/mine/credit_extension_page']
+
+    // if (window.history && window.history.pushState) {
+    //     if (home.includes(location.pathname)) {
+    //         $(window).on('popstate', function () {
+    //             window.history.pushState('forward', null, '#');
+    //             window.history.forward(1);
+    //         });
+    //     }
+
+    // }
+    // if (home.includes(location.pathname)) {
+    //     window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
+    //     window.history.forward(1);
+    // }
 }
