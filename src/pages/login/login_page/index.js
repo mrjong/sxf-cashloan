a2832d82ce24e8534d4950ef00e29a9713d90ea9
张晋佩 <302784355@qq.com>
2018-08-13 13:08:31 +0800

    修改登录页键盘遮住输入框问题

--- src/pages/login/login_page/index.js
+++ src/pages/login/login_page/index.js
@@ -1,3 +1,5 @@
+import qs from 'qs';
+import { address } from 'utils/Address';
 import React, { PureComponent } from 'react';
 import { createForm } from 'rc-form';
 import { Toast, InputItem } from 'antd-mobile';
@@ -6,10 +8,8 @@ import fetch from 'sx-fetch';
 import { store, getDeviceType, getFirstError } from 'utils/common';
 import { validators } from 'utils/validator';
 import style from './index.scss';
-import { address } from 'utils/Address'
-import qs from 'qs'
 
-let timmer
+let timmer;
 const API = {
   smsForLogin: '/signup/smsForLogin',
   sendsms: '/cmm/sendsms',
@@ -17,7 +17,6 @@ const API = {
 
 @fetch.inject()
 @createForm()
-
 export default class login_page extends PureComponent {
   constructor(props) {
     super(props);
@@ -30,17 +29,17 @@ export default class login_page extends PureComponent {
   }
 
   componentWillMount() {
-    document.title = "登录和注册"
+    document.title = '登录和注册';
     // 移除cookie
     Cookie.remove('fin-v-card-token');
-    sessionStorage.clear()
+    sessionStorage.clear();
     this.props.form.getFieldProps('phoneValue');
     this.props.form.setFieldsValue({
       phoneValue: '',
     });
   }
   componentDidMount() {
-    address()
+    address();
   }
 
   componentWillUnmount() {
@@ -60,30 +59,34 @@ export default class login_page extends PureComponent {
   goLogin = () => {
     const osType = getDeviceType();
     if (!this.state.smsJrnNo) {
-      Toast.info('请先获取短信验证码')
-      return
+      Toast.info('请先获取短信验证码');
+      return;
     }
     const queryData = qs.parse(this.props.history.location.search, { ignoreQueryPrefix: true });
     this.props.form.validateFields((err, values) => {
       if (!err) {
         this.props.$fetch.post(API.smsForLogin, {
-          mblNo: values.phoneValue, // 手机号
-          smsJrnNo: this.state.smsJrnNo, // 短信流水号
-          osType: osType, // 操作系统
-          smsCd: values.smsCd, // IP地址
-          usrCnl: queryData && queryData.h5Channel ? queryData.h5Channel : 'h5', // 用户渠道
-          location: store.getPosition(), // 定位地址 TODO 从session取
-        }).then(res => {
-          if (res.msgCode !== 'PTM0000') {
-            res.msgInfo && Toast.info(res.msgInfo);
-            return;
-          }
-          Cookie.set('fin-v-card-token', res.data.tokenId, { expires: 365 });
-          store.setToken(res.data.tokenId)
-          this.props.history.push('/home/home');
-        }, err => {
-          err.msgInfo && Toast.info(err.msgInfo);
-        });
+            mblNo: values.phoneValue, // 手机号
+            smsJrnNo: this.state.smsJrnNo, // 短信流水号
+            osType, // 操作系统
+            smsCd: values.smsCd, // IP地址
+            usrCnl: queryData && queryData.h5Channel ? queryData.h5Channel : 'h5', // 用户渠道
+            location: store.getPosition(), // 定位地址 TODO 从session取
+          })
+          .then(
+            res => {
+              if (res.msgCode !== 'PTM0000') {
+                res.msgInfo && Toast.info(res.msgInfo);
+                return;
+              }
+              Cookie.set('fin-v-card-token', res.data.tokenId, { expires: 365 });
+              store.setToken(res.data.tokenId);
+              this.props.history.push('/home/home');
+            },
+            error => {
+              error.msgInfo && Toast.info(error.msgInfo);
+            },
+          );
       } else {
         Toast.info(getFirstError(err));
       }
@@ -107,15 +110,16 @@ export default class login_page extends PureComponent {
       if (!err || JSON.stringify(err) === '{}') {
         // 发送验证码
         this.props.$fetch.post(API.sendsms, {
-          type: '6',
-          mblNo: values.phoneValue,
-          osType: osType,
-        }).then((result) => {
-          if (result.msgCode !== 'PTM0000') {
-            Toast.info(result.msgInfo);
-            this.setState({ valueInputImgCode: '' });
-            return false;
-          } else {
+            type: '6',
+            mblNo: values.phoneValue,
+            osType,
+          })
+          .then(result => {
+            if (result.msgCode !== 'PTM0000') {
+              Toast.info(result.msgInfo);
+              this.setState({ valueInputImgCode: '' });
+              return false;
+            }
             Toast.info('发送成功，请注意查收！');
             this.setState({ timeflag: false, smsJrnNo: result.data.smsJrnNo });
             timmer = setInterval(() => {
@@ -125,25 +129,41 @@ export default class login_page extends PureComponent {
                 this.setState({ timers: '重新获取', timeflag: true, flag: true });
               }
             }, 1000);
-
-          }
-        });
+          });
       } else {
         Toast.info(getFirstError(err));
       }
     });
   }
   // 跳转协议
-  go = (url) => {
-    this.props.history.push(`/protocol/${url}`)
-  }
+  go = url => {
+    this.props.history.push(`/protocol/${url}`);
+  };
+
+  // 处理键盘挡住输入框
+  handleScrollToView = id => {
+    // $('#loginWrap').scrollTop($('#loginContent').height());
+    $('#loginWrap').animate({
+      scrollTop: $('#loginContent').height(),
+    }, 300);
+    setTimeout(() => {
+      $('#loginWrap').animate({
+        scrollTop: $('#loginContent').height(),
+      }, 300);
+      // $('#loginWrap').scrollTop($('#loginContent').height());
+      $(id).focus();
+    }, 100);
+  };
+
   render() {
     const { getFieldProps } = this.props.form;
     return (
-      <div className={style.loginContent}>
-        <div className={style.loginLog}>
+      <div id="loginWrap" className={style.loginContent}>
+        <div id="loginContent" className={style.loginLog}>
           <div className={style.centent}>
             <InputItem
+              id="inputPhone"
+              onFocus={() => { this.handleScrollToView('inputPhone') }}
               maxLength="11"
               type="number"
               className={style.loginInput}
@@ -156,10 +176,12 @@ export default class login_page extends PureComponent {
               })}
             />
             <InputItem
+              id="inputCode"
+              onFocus={() => { this.handleScrollToView('inputCode') }}
               type="number"
               maxLength="6"
               className={style.loginInput}
-              placeholder='请输入短信验证码'
+              placeholder="请输入短信验证码"
               {...getFieldProps('smsCd', {
                 rules: [
                   { required: true, message: '请输入正确验证码' },
@@ -175,7 +197,13 @@ export default class login_page extends PureComponent {
             <div className={style.sureBtn} onClick={this.goLogin}>一键代还</div>
 
             <div className={style.agreement}>
-              注册即视为同意 <span onClick={() => { this.go('register_agreement_page') }}>《注册及授信协议》</span><span onClick={() => { this.go('privacy_agreement_page') }}>《用户隐私协议》</span>
+              注册即视为同意
+              <span onClick={() => { this.go('register_agreement_page') }}>
+                《注册及授信协议》
+              </span>
+              <span onClick={() => { this.go('privacy_agreement_page') }}>
+                《用户隐私协议》
+              </span>
             </div>
           </div>
         </div>
