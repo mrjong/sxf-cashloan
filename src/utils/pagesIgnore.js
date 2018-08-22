09db31ecaab6178f7a8bcd2623ddfb685cfcf170
FloweringVivian <flowering_season@163.com>
2018-08-22 16:51:19 +0800

    修改该页面不校验token

--- src/utils/pagesIgnore.js
+++ src/utils/pagesIgnore.js
@@ -1,6 +1,6 @@
 export default (pathname = window.location.pathname) => {
   if (pathname) {
-    let pageList = ['/protocol/'];
+    let pageList = ['/protocol/', '/common/auth_page'];
     return pageList.some(item => item && pathname.indexOf(item) > -1);
   }
 };
