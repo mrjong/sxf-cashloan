29df4ca408f0d66be204e6c42aab8f92189114e6
张晋佩 <302784355@qq.com>
2018-09-04 21:43:51 +0800

    添加落地页不校验登录

--- src/utils/pagesIgnore.js
+++ src/utils/pagesIgnore.js
@@ -1,6 +1,6 @@
 export default (pathname = window.location.pathname) => {
   if (pathname) {
-    let pageList = ['/protocol/', '/common/auth_page'];
+    let pageList = ['/protocol/', '/common/auth_page', '/landing/landing_page'];
     return pageList.some(item => item && pathname.indexOf(item) > -1);
   }
 };
