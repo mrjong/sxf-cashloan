2d3e99e41e941272c207ac284c7f275e76d35378
孙建坤 <sun_jk@suixingpay.com>
2018-09-26 11:10:12 +0800

    添加注释

--- src/pages/order/order_detail_page/index.js
+++ src/pages/order/order_detail_page/index.js
@@ -206,6 +206,7 @@ export default class order_detail_page extends PureComponent {
         let couponId = '';
         if (this.state.couponInfo && this.state.couponInfo.usrCoupNo) {
             if (this.state.couponInfo.usrCoupNo !== 'null') {
+                // 首末期利息为0时coupId为空
                 if (parseFloat(this.state.perdList[0].perdItrtAmt) === 0 || parseFloat(this.state.perdList[this.state.perdList.length - 1].perdItrtAmt) === 0) {
                     couponId = '';
                 } else {
@@ -217,6 +218,7 @@ export default class order_detail_page extends PureComponent {
 
         } else {
             if (this.state.billDesc.data && this.state.billDesc.data.coupVal) {
+                // 首末期利息为0时coupId为空
                 if (parseFloat(this.state.perdList[0].perdItrtAmt) === 0 || parseFloat(this.state.perdList[this.state.perdList.length - 1].perdItrtAmt) === 0) {
                     couponId = '';
                 } else {
@@ -224,7 +226,6 @@ export default class order_detail_page extends PureComponent {
                 }
             }
         }
-        console.log(couponId);
         this.props.$fetch.post(API.payback, {
             billNo: this.state.billNo,
             thisRepTotAmt: this.state.sendMoney,
