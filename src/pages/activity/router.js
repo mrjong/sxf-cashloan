a651763a2f66fe200d439da7e8c9d08c5e31134b
shawn2016 <787990963@qq.com>
2018-12-05 15:14:07 +0800

    微信登陆

--- src/pages/activity/router.js
+++ src/pages/activity/router.js
@@ -10,5 +10,12 @@ export default [
 		title: '全民免息狂欢',
 		arrowHide: true,
 		component: () => import('pages/activity/huodong11_page')
+    },
+    {
+		path: '/activity/wxshare',
+        title: '微信分享',
+        arrowHide: 'empty',
+        headerHide: true,
+		component: () => import('pages/activity/wxshare')
 	}
 ];
