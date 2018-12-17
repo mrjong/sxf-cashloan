b0cb1274581bf499f8ae43c3d85b2fd13c5d9fef
shawn2016 <787990963@qq.com>
2018-12-17 10:31:06 +0800

    测试

--- src/pages/activity/wxshare/index.js
+++ src/pages/activity/wxshare/index.js
@@ -84,10 +84,6 @@ export default class dc_landing_page extends PureComponent {
 							desc: '还到很牛x',
 							link: _this.state.href,
 							imgUrl: 'https://lns-static-resource.vbillbank.com/cashloan/wxapp_static/black_logo_2x.png',
-							trigger: function(res) {
-								// 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-								alert('用户点击分享到朋友圈');
-							},
 							success: function() {
 								// _this.doInvite();
 								Toast.info('分享成功');
