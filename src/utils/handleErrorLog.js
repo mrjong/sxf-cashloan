081eb8d0bbf3ec93d1bf82d0d9754b89394b1086
chji0ng <chji0ng@163.com>
2018-12-18 13:33:38 +0800

    '接口错误上报'

--- src/utils/handleErrorLog.js
+++ src/utils/handleErrorLog.js
@@ -6,7 +6,7 @@ export default function(error) {
   const logInfo = {
   	api_log_status:status,
   	api_log_statusText:statusText,
-  	api_log_url: config&&config.url || url,
+  	api_log_url: config && config.url || url,
   	api_log_time: new Date()
   }
    buriedPointEvent(bug_log.api_error_log, logInfo)
