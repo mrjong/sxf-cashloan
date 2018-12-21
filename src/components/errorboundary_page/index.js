2872110fb04df767994f724274653806772fc905
chenjiong <chenjiong@chenjiongdeMacBook-Pro.local>
2018-12-21 17:24:22 +0800

    错误日志上报

--- src/components/errorboundary_page/index.js
+++ src/components/errorboundary_page/index.js
@@ -20,7 +20,9 @@ export default class ErrorBoundary extends React.Component {
 		// 此处埋点上报页面、组件层级发生的错误
 		// console.log(errorInfo.componentStack)
 		buriedPointEvent(bug_log.api_error_log, {
-			errorLog: errorInfo.componentStack
+			errorLog: errorInfo.componentStack,
+			errorPage: document.title,
+			errorTime: new Date()
 		})
 	}
 
