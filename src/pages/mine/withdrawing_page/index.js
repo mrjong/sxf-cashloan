0e82434e7457e4353908ca5f1dcb05c23322a971
孙建坤 <sun_jk@suixingpay.com>
2019-01-03 11:26:19 +0800

    修改文件夹命名

--- src/pages/mine/withdrawing_page/index.js
+++ src/pages/mine/withdrawing_page/index.js
@@ -1,19 +1,12 @@
 import React, { PureComponent } from 'react';
 import style from './index.scss';
 import fetch from 'sx-fetch';
-import qs from 'qs';
-import dayjs from 'dayjs';
-import {setBackGround} from 'utils/Background'
-import Countdown from "./components/countdown"
-
-import { Toast } from 'antd-mobile';
+import Countdown from "./components/Countdown"
 
 let applyNo = ''; // 订单号
 let accountNum = ''; // 提现金额
-const API = {
-};
+
 @fetch.inject()
-// @setBackGround('#efeff4')
 export default class withdrawing_page extends PureComponent {
   constructor(props) {
     super(props);
