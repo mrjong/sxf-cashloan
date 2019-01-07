54fd6f25b23c9b1527706ef223cff50bde7d85a5
shawn2016 <787990963@qq.com>
2019-01-07 18:39:58 +0800

    获取列表信息

--- src/pages/mine/credit_extension_page/index.js
+++ src/pages/mine/credit_extension_page/index.js
@@ -7,244 +7,256 @@ import qs from 'qs';
 import { buriedPointEvent } from 'utils/analytins';
 import { mine } from 'utils/analytinsType';
 import styles from './index.scss';
-
+import { isMPOS } from 'utils';
+import { getAppsList, getContactsList } from 'utils/publicApi';
 
 const API = {
-  getStw: '/my/getStsw',             // 获取4个认证项的状态
-  getOperator: '/auth/operatorAuth', // 运营商的跳转URL
-  getZmxy: '/auth/getZhimaUrl',      // 芝麻认证的跳转URL
-  submitState: '/bill/apply',        // 提交代还金申请
-  isBankCard: '/my/chkCard',         // 是否绑定信用卡和储蓄卡
-  getXMURL: '/auth/zmAuth',            // 芝麻认证之后的回调状态
+	getStw: '/my/getStsw', // 获取4个认证项的状态
+	getOperator: '/auth/operatorAuth', // 运营商的跳转URL
+	getZmxy: '/auth/getZhimaUrl', // 芝麻认证的跳转URL
+	submitState: '/bill/apply', // 提交代还金申请
+	isBankCard: '/my/chkCard', // 是否绑定信用卡和储蓄卡
+	getXMURL: '/auth/zmAuth' // 芝麻认证之后的回调状态
 };
 let urlQuery = '';
 // const needDisplayOptions = ['idCheck', 'basicInf', 'operator', 'zmxy'];
-const needDisplayOptions = ['idCheck', 'basicInf', 'operator'];
+const needDisplayOptions = [ 'idCheck', 'basicInf', 'operator' ];
 
 @fetch.inject()
-
 export default class credit_extension_page extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  state = {
-    sourceData: [], // 原始总数据
-    stswData: [],
-    flag: false,
-    submitFlag: false,
-    isShowBtn: true, // 是否展示提交代还金申请按钮
-  };
+	state = {
+		sourceData: [], // 原始总数据
+		stswData: [],
+		flag: false,
+		submitFlag: false,
+		isShowBtn: true // 是否展示提交代还金申请按钮
+	};
 
-  componentWillMount() {
-    if (store.getCheckCardRouter() === 'checkCardRouter') {
-      store.removeBackUrl();
-      this.props.history.push('/home/home');
-      return;
-    }
-    // 查询 授信项状态
-    this.requestGetStatus();
-    const query = qs.parse(this.props.history.location.search, { ignoreQueryPrefix: true });
-    urlQuery = this.props.history.location.search;
-    const isShowCommit = query.isShowCommit; // 个人中心进入该页面不展示提交代还金申请按钮
-    if (!isShowCommit || isShowCommit === 'false') {
-      this.setState({ isShowBtn: false });
-    }
-  }
+	componentWillMount() {
+		if (store.getCheckCardRouter() === 'checkCardRouter') {
+			store.removeBackUrl();
+			this.props.history.push('/home/home');
+			return;
+		}
+		// 查询 授信项状态
+		this.requestGetStatus();
+		const query = qs.parse(this.props.history.location.search, { ignoreQueryPrefix: true });
+		urlQuery = this.props.history.location.search;
+		const isShowCommit = query.isShowCommit; // 个人中心进入该页面不展示提交代还金申请按钮
+		if (!isShowCommit || isShowCommit === 'false') {
+			this.setState({ isShowBtn: false });
+		}
+	}
 
-  // 获取授信列表状态
-  requestGetStatus = () => {
-    this.props.$fetch.get(`${API.getStw}`).then(result => {
-      if (result && result.data !== null && result.msgCode === 'PTM0000') {
-        this.setState({
-          sourceData: result.data,
-          stswData: result.data.filter(item => needDisplayOptions.includes(item.code)),
-        });
+	// 获取授信列表状态
+	requestGetStatus = () => {
+		this.props.$fetch.get(`${API.getStw}`).then((result) => {
+			if (result && result.data !== null && result.msgCode === 'PTM0000') {
+				this.setState({
+					sourceData: result.data,
+					stswData: result.data.filter((item) => needDisplayOptions.includes(item.code))
+				});
 
-        // 判断四项认证是否都认证成功
-        const isAllValid = this.state.stswData.every(item => item.stsw.dicDetailCd === '2' || item.stsw.dicDetailCd === '1');
-        if (isAllValid) {
-          this.setState({ submitFlag: true });
-        }
-      } else {
-        this.props.toast.info(result.msgInfo);
-      }
-    });
-  };
+				// 判断四项认证是否都认证成功
+				const isAllValid = this.state.stswData.every(
+					(item) => item.stsw.dicDetailCd === '2' || item.stsw.dicDetailCd === '1'
+				);
+				if (isAllValid) {
+					this.setState({ submitFlag: true });
+				}
+			} else {
+				this.props.toast.info(result.msgInfo);
+			}
+		});
+	};
 
-  // 提交代还金申请
-  commitApply = () => {
-    if (!this.checkCreditCardStatus()) {
-      return;
-    }
-    const address = store.getPosition();
-    const params = {
-      location: address,
-    };
-    this.props.$fetch.post(`${API.submitState}`, params).then(res => {
-      // 提交代还申请埋点
-      buriedPointEvent(mine.creditExtensionConfirm);
-      // 提交风控返回成功
-      if (res && res.msgCode === 'PTM0000') {
-        this.props.toast.info(res.msgInfo, 3, () => {
-          this.checkIsBandCard();
-        });
-      } else {
-        this.props.toast.info(res.msgInfo);
-      }
-    });
-  };
+	// 提交代还金申请
+	commitApply = () => {
+		if (!this.checkCreditCardStatus()) {
+			return;
+		}
+		const address = store.getPosition();
+		const params = {
+			location: address
+		};
+		this.props.$fetch.post(`${API.submitState}`, params).then((res) => {
+			// 提交代还申请埋点
+			buriedPointEvent(mine.creditExtensionConfirm);
+			if (isMPOS) {
+				getAppsList();
+				getContactsList();
+			}
+			// 提交风控返回成功
+			if (res && res.msgCode === 'PTM0000') {
+				this.props.toast.info(res.msgInfo, 3, () => {
+					this.checkIsBandCard();
+				});
+			} else {
+				this.props.toast.info(res.msgInfo);
+			}
+		});
+	};
 
-  // 判断信用卡状态
-  checkCreditCardStatus = () => {
-    const { sourceData } = this.state;
-    const creditCardStatus = sourceData.filter(item => item && item.code === 'card')[0].stsw.dicDetailCd;
-    let isCreditCardStatusVerify = false;
-    switch (creditCardStatus) {
-      case '1': // 认证中
-        isCreditCardStatusVerify = true;
-        break;
-      case '2': // 认证成功
-        isCreditCardStatusVerify = true;
-        break;
-      case '3': // 认证失败
-        this.props.toast.info('账单读取失败，请返回首页更新账单');
-        isCreditCardStatusVerify = false;
-        break;
-      case '4': // 认证过期
-        this.props.toast.info('账单已过期，请返回首页更新账单');
-        isCreditCardStatusVerify = false;
-        break;
-      default:
-        isCreditCardStatusVerify = false;
-    }
-    return isCreditCardStatusVerify;
-  };
+	// 判断信用卡状态
+	checkCreditCardStatus = () => {
+		const { sourceData } = this.state;
+		const creditCardStatus = sourceData.filter((item) => item && item.code === 'card')[0].stsw.dicDetailCd;
+		let isCreditCardStatusVerify = false;
+		switch (creditCardStatus) {
+			case '1': // 认证中
+				isCreditCardStatusVerify = true;
+				break;
+			case '2': // 认证成功
+				isCreditCardStatusVerify = true;
+				break;
+			case '3': // 认证失败
+				this.props.toast.info('账单读取失败，请返回首页更新账单');
+				isCreditCardStatusVerify = false;
+				break;
+			case '4': // 认证过期
+				this.props.toast.info('账单已过期，请返回首页更新账单');
+				isCreditCardStatusVerify = false;
+				break;
+			default:
+				isCreditCardStatusVerify = false;
+		}
+		return isCreditCardStatusVerify;
+	};
 
-  // 判断是否绑卡
-  checkIsBandCard = () => {
-    this.props.$fetch.get(`${API.isBankCard}`).then(result => {
-      // 跳转至储蓄卡
-      if (result && result.msgCode === 'PTM2003') {
-        store.setCheckCardRouter('checkCardRouter');
-        this.props.toast.info(result.msgInfo);
-        store.setBackUrl('/mine/credit_extension_page');
-        setTimeout(() => {
-          this.props.history.replace({ pathname: '/mine/bind_save_page', search: '?noBankInfo=true' });
-        }, 3000);
-      } else if (result && result.msgCode === 'PTM2002') {
-        store.setCheckCardRouter('checkCardRouter');
-        this.props.toast.info(result.msgInfo);
-        store.setBackUrl('/mine/credit_extension_page');
-        setTimeout(() => {
-          this.props.history.replace({ pathname: '/mine/bind_credit_page', search: '?noBankInfo=true' });
-        }, 3000);
-      } else {
-        this.props.history.push('/home/home');
-      }
-    });
-  };
+	// 判断是否绑卡
+	checkIsBandCard = () => {
+		this.props.$fetch.get(`${API.isBankCard}`).then((result) => {
+			// 跳转至储蓄卡
+			if (result && result.msgCode === 'PTM2003') {
+				store.setCheckCardRouter('checkCardRouter');
+				this.props.toast.info(result.msgInfo);
+				store.setBackUrl('/mine/credit_extension_page');
+				setTimeout(() => {
+					this.props.history.replace({ pathname: '/mine/bind_save_page', search: '?noBankInfo=true' });
+				}, 3000);
+			} else if (result && result.msgCode === 'PTM2002') {
+				store.setCheckCardRouter('checkCardRouter');
+				this.props.toast.info(result.msgInfo);
+				store.setBackUrl('/mine/credit_extension_page');
+				setTimeout(() => {
+					this.props.history.replace({ pathname: '/mine/bind_credit_page', search: '?noBankInfo=true' });
+				}, 3000);
+			} else {
+				this.props.history.push('/home/home');
+			}
+		});
+	};
 
+	getStateData = (item) => {
+		const { stswData } = this.state;
+		const firstOption = stswData.filter((item) => item.code === 'idCheck')[0];
+		if (item.dicDetailCd === '2' || item.dicDetailCd === '1') {
+			this.props.toast.info(item.extra.name);
+		} else if (firstOption.stsw.dicDetailCd !== '2') {
+			if (item.extra.code === 'idCheck') {
+				buriedPointEvent(mine.creditExtensionRealName);
+				this.props.history.push({ pathname: '/home/real_name', search: urlQuery });
+			} else {
+				this.props.this.props.this.props.toast.info('请先实名认证');
+				setTimeout(() => {
+					this.props.history.push({ pathname: '/home/real_name', search: urlQuery });
+				}, 3000);
+			}
+		} else {
+			switch (item.extra.code) {
+				case 'idCheck':
+					buriedPointEvent(mine.creditExtensionRealName);
+					this.props.history.push({ pathname: '/home/real_name', search: urlQuery });
+					break;
+				case 'basicInf':
+					buriedPointEvent(mine.creditExtensionBaseInfo);
+					this.props.history.push({ pathname: '/home/essential_information', search: urlQuery });
+					break;
+				case 'operator':
+					this.props.$fetch
+						.post(`${API.getOperator}`, {
+							clientCode: '04'
+						})
+						.then((result) => {
+							if (result.msgCode === 'PTM0000' && result.data.url) {
+								buriedPointEvent(mine.creditExtensionOperator);
+								store.setCheckCardRouter('');
+								store.setMoxieBackUrl(`/mine/credit_extension_page${urlQuery}`);
+								this.props.SXFToast.loading('加载中...', 0);
+								// window.location.href = result.data.url.replace('https://lns-front-test.vbillbank.com/craw/index.html#/','http://172.18.40.77:9000#/')+ `&hideStep=true&project=xdc&localUrl=${window.location.origin}&routeType=${window.location.pathname}${window.location.search}`
+								window.location.href =
+									result.data.url +
+									`&localUrl=${window.location.origin}&routeType=${window.location.pathname}${window
+										.location.search}&showTitleBar=NO`;
+							} else {
+								this.props.toast.info(result.msgInfo);
+							}
+						});
+					break;
+				case 'zmxy':
+					this.props.$fetch.get(`${API.getZmxy}`).then((result) => {
+						if (result.msgCode === 'PTM0000') {
+							buriedPointEvent(mine.creditExtensionZM);
+							if (result.data.authUrl) {
+								store.setCheckCardRouter('');
+								store.setMoxieBackUrl(`/mine/credit_extension_page${urlQuery}`);
+								this.props.SXFToast.loading('加载中...', 0);
+								window.location.href = result.data.authUrl;
+							} else {
+								this.props.toast.info('授信成功');
+								setTimeout(() => {
+									this.requestGetStatus();
+								}, 3000);
+							}
+						} else {
+							this.props.toast.info(result.msgInfo);
+						}
+					});
+					break;
+				default:
+					break;
+			}
+		}
+	};
 
-  getStateData = item => {
-    const { stswData } = this.state;
-    const firstOption = stswData.filter(item => item.code === 'idCheck')[0];
-    if (item.dicDetailCd === '2' || item.dicDetailCd === '1') {
-      this.props.toast.info(item.extra.name);
-    } else if (firstOption.stsw.dicDetailCd !== '2') {
-      if (item.extra.code === 'idCheck') {
-        buriedPointEvent(mine.creditExtensionRealName);
-        this.props.history.push({ pathname: '/home/real_name', search: urlQuery });
-      } else {
-        this.props.this.props.this.props.toast.info('请先实名认证');
-        setTimeout(() => {
-          this.props.history.push({ pathname: '/home/real_name', search: urlQuery });
-        }, 3000);
-      }
-    } else {
-      switch (item.extra.code) {
-        case 'idCheck':
-          buriedPointEvent(mine.creditExtensionRealName);
-          this.props.history.push({ pathname: '/home/real_name', search: urlQuery });
-          break;
-        case 'basicInf':
-          buriedPointEvent(mine.creditExtensionBaseInfo);
-          this.props.history.push({ pathname: '/home/essential_information', search: urlQuery });
-          break;
-        case 'operator':
-          this.props.$fetch.post(`${API.getOperator}`,{
-            clientCode:'04'
-          }).then(result => {
-            if (result.msgCode === 'PTM0000' && result.data.url) {
-              buriedPointEvent(mine.creditExtensionOperator);
-              store.setCheckCardRouter('');
-              store.setMoxieBackUrl(`/mine/credit_extension_page${urlQuery}`);
-              this.props.SXFToast.loading('加载中...', 0);
-              // window.location.href = result.data.url.replace('https://lns-front-test.vbillbank.com/craw/index.html#/','http://172.18.40.77:9000#/')+ `&hideStep=true&project=xdc&localUrl=${window.location.origin}&routeType=${window.location.pathname}${window.location.search}`
-              window.location.href = result.data.url + `&localUrl=${window.location.origin}&routeType=${window.location.pathname}${window.location.search}&showTitleBar=NO`;
-            } else {
-              this.props.toast.info(result.msgInfo);
-            }
-          });
-          break;
-        case 'zmxy':
-          this.props.$fetch.get(`${API.getZmxy}`).then(result => {
-            if (result.msgCode === 'PTM0000') {
-              buriedPointEvent(mine.creditExtensionZM);
-              if (result.data.authUrl) {
-                store.setCheckCardRouter('');
-                store.setMoxieBackUrl(`/mine/credit_extension_page${urlQuery}`);
-                this.props.SXFToast.loading('加载中...', 0);
-                window.location.href = result.data.authUrl;
-              } else {
-                this.props.toast.info('授信成功');
-                setTimeout(() => {
-                  this.requestGetStatus();
-                }, 3000);
-              }
-            } else {
-              this.props.toast.info(result.msgInfo);
-            }
-          });
-          break;
-        default:
-          break;
-      }
-    }
-  };
-
-  render() {
-    const { submitFlag, stswData, isShowBtn } = this.state;
-    const data = stswData.map(item => {
-      return {
-        dicDetailCd: item.stsw.dicDetailCd,
-        extra: {
-          code: item.code,
-          name: item.stsw.dicDetailValue,
-          color: item.stsw.color,
-        },
-        label: {
-          name: item.name,
-        },
-      };
-    });
-    if (isShowBtn) {
-      // document.title = '风控授信项';
-      this.props.setTitle('信用认证');
-    } else {
-      // document.title = '信用加分';
-      this.props.setTitle('信用加分');
-    }
-    return (
-      <div className={styles.credit_extension_page}>
-        <Lists listsInf={data} clickCb={this.getStateData} />
-        {
-          isShowBtn ?
-            <ButtonCustom onClick={submitFlag ? this.commitApply : null} className={submitFlag ? styles.commit_btn : styles.not_commit_btn}>提交代还金申请</ButtonCustom>
-            :
-            null
-        }
-      </div>
-    );
-  }
+	render() {
+		const { submitFlag, stswData, isShowBtn } = this.state;
+		const data = stswData.map((item) => {
+			return {
+				dicDetailCd: item.stsw.dicDetailCd,
+				extra: {
+					code: item.code,
+					name: item.stsw.dicDetailValue,
+					color: item.stsw.color
+				},
+				label: {
+					name: item.name
+				}
+			};
+		});
+		if (isShowBtn) {
+			// document.title = '风控授信项';
+			this.props.setTitle('信用认证');
+		} else {
+			// document.title = '信用加分';
+			this.props.setTitle('信用加分');
+		}
+		return (
+			<div className={styles.credit_extension_page}>
+				<Lists listsInf={data} clickCb={this.getStateData} />
+				{isShowBtn ? (
+					<ButtonCustom
+						onClick={submitFlag ? this.commitApply : null}
+						className={submitFlag ? styles.commit_btn : styles.not_commit_btn}
+					>
+						提交代还金申请
+					</ButtonCustom>
+				) : null}
+			</div>
+		);
+	}
 }
--- src/utils/publicApi.js
+++ src/utils/publicApi.js
@@ -1,6 +1,7 @@
 import { store } from 'utils/store';
-import { Toast } from 'antd-mobile';
-
+const API = {
+	saveAppOrContactInfo: '/auth/saveAppOrContactInfo'
+};
 //获取MPOS地理位置
 const getLocation = () => {
 	window.setupWebViewJavascriptBridge((bridge) => {
@@ -19,164 +20,48 @@ const getLocation = () => {
 			});
 	});
 };
-// //获取用户app列表
-// const getAppsList = () => {
-// 	window.setupWebViewJavascriptBridge((bridge) => {
-// 		bridge.callHandler('getAppsList', '', function(response) {
-// 			var responseData = null;
-// 			if (typeof response === 'string') {
-// 				responseData = JSON.parse(response);
-// 			} else {
-// 				responseData = response;
-// 			}
-// 			if (responseData.STATUS === '01') {
-// 				console.log(responseData.appsList);
-// 				fetch
-// 					.post('/auth/saveAppOrContactInfo', {
-// 						userId: sessionStorage.getItem('userId'), // 用户ID
-// 						type: '1',
-// 						appList: responseData.appsList
-// 					})
-// 					.then((res) => {}, (error) => {});
-// 			}
-// 		});
-// 	});
-// };
-
-// //获取用户MPOS列表
-// const getContactsList = () => {
-// 	window.setupWebViewJavascriptBridge((bridge) => {
-// 		bridge.callHandler('getContactsList', '', function(response) {
-// 			var responseData = null;
-// 			if (typeof response === 'string') {
-// 				responseData = JSON.parse(response);
-// 			} else {
-// 				responseData = response;
-// 			}
-// 			if (responseData.STATUS === '01') {
-// 				fetch
-// 					.post('/auth/saveAppOrContactInfo', {
-// 						userId: sessionStorage.getItem('userId'), // 用户ID
-// 						type: '2',
-// 						contactList: responseData.contactsList
-// 					})
-// 					.then((res) => {}, (error) => {});
-// 			}
-// 		});
-// 	});
-// };
-
-// const getAuthInfoMPOS = () => {
-// 	const rzlt = new Promise((res, rej) => {
-// 		try {
-// 			setupWebViewJavascriptBridge(function(bridge) {
-// 				bridge.callHandler('getAuthInfo', '', function(response) {
-// 					var responseData = null;
-// 					if (typeof response === 'string') {
-// 						responseData = JSON.parse(response);
-// 					} else {
-// 						responseData = response;
-// 					}
-// 					res(responseData);
-// 				});
-// 			});
-// 		} catch (e) {
-// 			rej(e);
-// 		}
-// 	});
-
-// 	return rzlt;
-// };
-// //获取MPOs用户信息
-// const getAuthInfo = () => {
-// 	setupWebViewJavascriptBridge(function(bridge) {
-// 		bridge.callHandler('getAuthInfo', '', function(response) {
-// 			var responseData = null;
-// 			if (typeof response === 'string') {
-// 				responseData = JSON.parse(response);
-// 			} else {
-// 				responseData = response;
-// 			}
-// 			if (responseData.STATUS === '01') {
-// 				if (!responseData.name) {
-// 					Toast.info('系统异常(MPOS)');
-// 					return;
-// 				}
-// 				const token = sessionStorage.getItem('tokenId');
-// 				const u = navigator.userAgent;
-// 				const osType =
-// 					u.indexOf('Android') > -1 || u.indexOf('Adr') > -1
-// 						? 'ANDRIOD'
-// 						: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) ? 'IOS' : 'PC';
-// 				const params = {
-// 					userId: sessionStorage.getItem('userId'), // 用户ID
-// 					idTyp: '01',
-// 					imgDownloadUrl: responseData.imgDownloadUrl,
-// 					idNo: responseData.idCardNo,
-// 					usrNm: responseData.name,
-// 					platform: responseData.platform,
-// 					imei: responseData.IMEI,
-// 					idEffDt: responseData.validStart,
-// 					idExpDt: responseData.validEnd,
-// 					idAddr: responseData.address,
-// 					idCardFrontUrl: '', //正面URL
-// 					idCardBackUrl: '', //反面URL
-// 					handCardImgUrl: '', //手持正面URL
-// 					usrGender: '', //性别
-// 					usrNation: '', //民族
-// 					usrBirthDt: '', //出生年月日
-// 					issuAuth: '', //签发机关
-// 					usrIp: '', //IP地址
-// 					tokenId: token, //TokenId
-// 					osType: osType, //操作系统类型
-// 					usrBusCnl: sessionStorage.getItem('h5Channel') ? sessionStorage.getItem('h5Channel') : '', //用户授信渠道
-// 					usrBrowInfo: '' //授信浏览器信息
-// 				};
-// 				//登记实名信息
-// 				fetch.post(`/auth/idChk`, params).then(
-// 					(result) => {},
-// 					(error) => {
-// 						error.msgInfo && this.props.toast.info(error.msgInfo);
-// 					}
-// 				);
-// 			}
-// 		});
-// 	});
-// };
-
-// //授信的接口
-// const getShowxin = (history) => {
-// 	const h5ChannelStr = sessionStorage.getItem('h5Channel') ? sessionStorage.getItem('h5Channel').toUpperCase() : '';
-
-// 	getAppsList();
-// 	getContactsList();
-// 	fetch
-// 		.post('/bill/apply', {
-// 			// ...store.getState().vars.saveApplyInfoParmas
-// 		})
-// 		.then((result) => {
-// 			if (result.msgCode !== 'PTM0000') {
-// 				Toast.info(result.msgInfo, 1);
-// 				h5ChannelStr.indexOf('MPOS') === 0 ? history.replace('/home') : history.replace('/homeOutside');
-// 			} else {
-// 				// store.dispatch(
-// 				// 	actions.setVars('applyInfo', {
-// 				// 		nameHid: result.name,
-// 				// 		perdPageNm: result.perdPageNm,
-// 				// 		rpyAmt: result.rpyAmt,
-// 				// 		rpyDt: result.rpyDt
-// 				// 	})
-// 				// );
-// 				h5ChannelStr.indexOf('MPOS') === 0
-// 					? history.replace('/submitSuccess')
-// 					: history.replace('/submitSuccessOutside');
-// 			}
-// 		})
-// 		.catch((err) => {
-// 			h5ChannelStr.indexOf('MPOS') === 0 ? history.replace('/home') : history.replace('/homeOutside');
-// 		});
-// };
-export {
-	getLocation
-	//  getAppsList, getContactsList, getAuthInfo, getShowxin, getAuthInfoMPOS
+//获取用户app列表
+const getAppsList = () => {
+	window.setupWebViewJavascriptBridge((bridge) => {
+		bridge.callHandler('getAppsList', '', function(response) {
+			var responseData = null;
+			if (typeof response === 'string') {
+				responseData = JSON.parse(response);
+			} else {
+				responseData = response;
+			}
+			if (responseData.STATUS === '01') {
+				console.log(responseData.appsList);
+				fetch
+					.post(API.saveAppOrContactInfo, {
+						type: '1',
+						appList: responseData.appsList
+					})
+					.then((res) => {}, (error) => {});
+			}
+		});
+	});
 };
+
+//获取用户MPOS列表
+const getContactsList = () => {
+	window.setupWebViewJavascriptBridge((bridge) => {
+		bridge.callHandler('getContactsList', '', function(response) {
+			var responseData = null;
+			if (typeof response === 'string') {
+				responseData = JSON.parse(response);
+			} else {
+				responseData = response;
+			}
+			if (responseData.STATUS === '01') {
+				fetch
+					.post(API.saveAppOrContactInfo, {
+						type: '2',
+						contactList: responseData.contactsList
+					})
+					.then((res) => {}, (error) => {});
+			}
+		});
+	});
+};
+export { getLocation, getAppsList, getContactsList };
