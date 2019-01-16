20a73dc03de5cc8d39127812b4d5921d28a0476a
孙建坤 <sun_jk@suixingpay.com>
2019-01-16 10:30:21 +0800

    修改登陆去掉session里渠道的bug

--- src/pages/login/login_page/index.js
+++ src/pages/login/login_page/index.js
@@ -44,11 +44,11 @@ export default class login_page extends PureComponent {
 		const query = qs.parse(window.location.search, {
 			ignoreQueryPrefix: true
 		});
+		setH5Channel();
 		// 移除cookie
 		Cookie.remove('fin-v-card-token');
 		sessionStorage.clear();
 		localStorage.clear();
-		setH5Channel();
 
 		store.setHistoryRouter(window.location.pathname);
 
