330bd50296f88766e54d9c312f24c2746981debd
chenjiong <kapp1e@163.com>
2019-03-05 14:15:40 +0800

    埋点优化

--- src/pages/home/confirm_agency_page/components/TagList/index.js
+++ src/pages/home/confirm_agency_page/components/TagList/index.js
@@ -29,20 +29,8 @@ export default class TagList extends React.PureComponent {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.burientype && !store.getHadShowModal()) {
-      const item = nextProps.tagList[nextProps.activeindex]
-      if (item.value === '1') {
-        // 预约还款埋点
-        buriedPointEvent(home.lendersOrder, {
-          lenders_type: item.name,
-          disable: item.disable || ''
-        })
-      } else {
-        // 立即还款埋点
-        buriedPointEvent(home.lenders, {
-          lenders_type: item.name,
-          disable: item.disable || ''
-        })
-      }
+      // const item = nextProps.tagList[nextProps.activeindex]
+      
       store.setHadShowModal(true)
     }
     if ((this.props.tagList.length !== nextProps.tagList.length) || (this.props.activeindex !== nextProps.activeindex)) {
@@ -96,6 +84,19 @@ export default class TagList extends React.PureComponent {
         className={className}
         active={!item.disable && index === currentIndex}
         onClick={() => {
+          if (item.value === '1') {
+            // 预约还款埋点
+            buriedPointEvent(home.lendersOrder, {
+              lenders_type: item.name,
+              disable: item.disable || ''
+            })
+          } else {
+            // 立即还款埋点
+            buriedPointEvent(home.lenders, {
+              lenders_type: item.name,
+              disable: item.disable || ''
+            })
+          }
           if (item.disable || index === currentIndex) {
             return;
           }
