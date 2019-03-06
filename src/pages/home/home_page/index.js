b9e4c026fd561bc0168d7c57abe018ae18e2c5ef
shawn2016 <787990963@qq.com>
2019-03-06 11:31:10 +0800

    --

--- src/pages/home/home_page/index.js
+++ src/pages/home/home_page/index.js
@@ -109,7 +109,11 @@ export default class home_page extends PureComponent {
 	calculatePercent = (list) => {
 		let codes = [];
 		list.forEach((element) => {
-			if (element.code === 'basicInf' || element.code === 'operator' || element.code === 'idCheck') {
+			if (
+				element.code === 'basicInf' ||
+				element.code === 'operator' ||
+				element.code === 'idCheck'
+			) {
 				codes.push(element.stsw.dicDetailCd);
 			}
 		});
@@ -119,7 +123,8 @@ export default class home_page extends PureComponent {
 		// case '4': // 认证过期
 		let newCodes2 = codes.filter((ele, index, array) => {
 			return ele === '1' || ele === '2';
-		});
+        });
+        console.log(newCodes2)
 		// 还差 2 步 ：三项认证项，完成任何一项认证项且未失效
 		// 还差 1 步 ：三项认证项，完成任何两项认证项且未失效
 		// 还差 0 步 ：三项认证项，完成任何三项认证项且未失效（不显示）
@@ -132,6 +137,11 @@ export default class home_page extends PureComponent {
 			case 2: // 新用户，信用卡未授权
 				this.setState({
 					percentSatus: '1'
+				});
+				break;
+			case 0: // 新用户，信用卡未授权
+				this.setState({
+					percentSatus: '3'
 				});
 				break;
 			default:
@@ -377,7 +387,7 @@ export default class home_page extends PureComponent {
 				Cookie.set('bannerAble', true, { expires: inFifteenMinutes });
 				store.setBannerData(bannerData);
 				this.setState({
-					bannerList: bannerData 
+					bannerList: bannerData
 				});
 			}
 		});
@@ -398,7 +408,7 @@ export default class home_page extends PureComponent {
 					this.getPercent();
 				}
 				this.setState({
-					usrIndexInfo: result.data.indexData 
+					usrIndexInfo: result.data.indexData
 						? result.data
 						: Object.assign({}, result.data, { indexData: {} })
 				});
@@ -511,6 +521,8 @@ export default class home_page extends PureComponent {
 								usrIndexInfo.indexSts === 'LN0006' ||
 								usrIndexInfo.indexSts === 'LN0008' ? (
 									'一键还账单'
+								) : usrIndexInfo.indexSts === 'LN0001' ? (
+									'查看我的账单，帮我还'
 								) : (
 									usrIndexInfo.indexMsg
 								)}
