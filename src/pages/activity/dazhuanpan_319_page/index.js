b7c576367e0e8f632b3d460300e0b3021aca80fd
shawn2016 <787990963@qq.com>
2019-03-14 10:58:01 +0800

    --

--- src/pages/activity/dazhuanpan_319_page/index.js
+++ src/pages/activity/dazhuanpan_319_page/index.js
@@ -4,26 +4,29 @@ import qs from 'qs';
 import styles from './index.scss';
 import AwardShow from './components/AwardShow';
 import RuleShow from './components/RuleShow';
+import { getDeviceType } from 'utils';
 import LoginAlert from './components/LoginAlert';
 import { setBackGround } from 'utils/background';
-import { Toast } from 'antd-mobile';
-import bg from './img/bg.png';
+import { Toast, Modal, Icon } from 'antd-mobile';
+import bg from './img/bg.jpg';
 import zp_bg from './img/zp_bg.png';
 import over from './img/over.png';
 import notstart from './img/notstart.png';
 import zp_btn from './img/zp_btn.png';
-import item1 from './img/item1.png';
 import config from './config.js';
 import Cookie from 'js-cookie';
 import { getH5Channel } from 'utils/common';
-import {store} from 'utils/store'
+import { store } from 'utils/store';
 
 const API = {
 	activeConfig: '/activeConfig/list', // 活动配置接口
 	awardRecords: '/activeConfig/records', // 用户中奖记录展示
 	recordForUser: '/activeConfig/recordForUser', // 用户中奖记录展示
 	userCount: '/activeConfig/count', // 用户抽奖次数查询
-	userDraw: '/activeConfig/draw' // 用户抽奖
+	userDraw: '/activeConfig/draw', // 用户抽奖
+	validateMposRelSts: '/authorize/validateMposRelSts',
+	chkAuth: '/authorize/chkAuth',
+	doAuth: '/authorize/doAuth'
 };
 let token = '';
 @fetch.inject()
@@ -42,11 +45,16 @@ export default class dazhuanpan_page extends PureComponent {
 			count: '1',
 			allUsersAward: [],
 			type: '', // 弹框类型
-			userAwardList: [] // 用户中奖列表
+			userAwardList: [], // 用户中奖列表
+			channel_value: '', // 那个渠道  mpos VS xdc
+			showLoginTip: false
 		};
 	}
 	isTurn = false;
 	componentWillMount() {
+		this.init();
+	}
+	init = () => {
 		const queryData = qs.parse(location.search, { ignoreQueryPrefix: true });
 		if (queryData.activeId) {
 			config.activeId = queryData.activeId;
@@ -62,15 +70,22 @@ export default class dazhuanpan_page extends PureComponent {
 				codeInfo: ''
 			});
 		}
-		token = Cookie.get('fin-v-card-token');
+		// 保存入口
+		if (queryData.entry) {
+			this.setState({
+				channel_value: queryData.entry
+			});
+		}
+		// 默认中奖次数显示
 		this.setState({
 			count:
 				store.getRewardCount() && Number(store.getRewardCount()) > 0
 					? store.getRewardCount()
 					: !store.getRewardCount() ? 1 : 0
 		});
+		// 初始化大转盘活动
 		this.getConfigList();
-	}
+	};
 	// 刷新大转盘数据
 	refreshPage = () => {
 		// this.getCount();
@@ -97,6 +112,7 @@ export default class dazhuanpan_page extends PureComponent {
 						case '大转盘':
 							// 是否展示其他用户抽奖记录  01是 00 否
 							if (res.data && res.data.data && res.data.data.recordShow !== '00') {
+								// 展示中奖记录
 								this.getAwardList('00');
 							}
 							this.setState({
@@ -138,10 +154,10 @@ export default class dazhuanpan_page extends PureComponent {
 				}
 			} else {
 				if (res.msgCode === 'PTM1000') {
-                    Cookie.remove('fin-v-card-token');
-                    this.onloadZhuan()
+					Cookie.remove('fin-v-card-token');
+					this.onloadZhuan();
 				} else if (res.msgCode === 'PTM0100') {
-                    this.onloadZhuan()
+					this.onloadZhuan();
 					Cookie.remove('fin-v-card-token');
 				} else {
 					Toast.info(res.msgInfo);
@@ -149,16 +165,125 @@ export default class dazhuanpan_page extends PureComponent {
 			}
 		});
 	};
+	isAuthFunc = (callBack) => {
+		const query = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+		let token = Cookie.get('fin-v-card-token');
+		if (token) {
+			callBack();
+			// 根据type 随便去干
+		} else {
+			if (query.entry && query.entry.indexOf('ismpos') > -1) {
+				// 去实名
+				this.validateMposRelSts();
+			} else {
+				// 新代偿
+				this.props.toast.info('请先登录', 2, () => {
+					this.props.history.push('/login');
+				});
+			}
+		}
+	};
+	// 实名
+	validateMposRelSts = () => {
+		const query = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+		if (query.appId && query.token) {
+			this.props.$fetch
+				.post(API.validateMposRelSts, {
+					appid: query.appId,
+					token: query.token
+				})
+				.then((res) => {
+					if (res.msgCode === 'URM0000') {
+						this.chkAuth();
+					} else {
+						this.props.toast.info('暂无活动资格');
+					}
+				})
+				.catch((err) => {
+					this.props.toast.info('暂无活动资格');
+				});
+		} else {
+			this.setState({ showLoginTip: true });
+		}
+	};
+
+	chkAuth = () => {
+		const query = qs.parse(this.props.history.location.search, { ignoreQueryPrefix: true });
+		this.props.$fetch
+			.post(API.chkAuth, {
+				mblNo: query.telNo,
+				appId: query.appId,
+				token: query.token,
+				location: store.getPosition(), // 定位地址 TODO 从session取,
+				osType: getDeviceType(),
+				province: '',
+				usrCnl: getH5Channel()
+			})
+			.then(
+				(res) => {
+					if (res.authFlag === '0') {
+						this.props.history.replace(
+							`/mpos/mpos_service_authorization_page?tokenId=${res.tokenId}&mblNoHid=${res.mblNoHid}`
+						);
+					} else if (res.authFlag === '1') {
+						// 已授权
+						Cookie.set('fin-v-card-token', res.loginToken, { expires: 365 });
+						store.setToken(res.loginToken);
+					} else {
+						this.setState({
+							showLoginTip: true
+						});
+					}
+				},
+				(err) => {
+					this.setState({
+						errorInf: '加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
+					});
+					console.log(err);
+				}
+			)
+			.catch((err) => {
+				this.setState({
+					errorInf: '加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
+				});
+				console.log(err);
+			});
+	};
+	// 去授权
+	doAuth = () => {
+		const query = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+		this.props.$fetch
+			.post(API.doAuth, {
+				location: store.getPosition(), // 定位地址 TODO 从session取,
+				osType: getDeviceType(),
+				authToken: query.tokenId
+			})
+			.then(
+				(res) => {
+					if (res.authSts === '01') {
+						console.log('发验证码');
+					} else if (res.authSts === '00') {
+						// sa.login(res.userId);
+						Cookie.set('fin-v-card-token', res.loginToken, { expires: 365 });
+						store.setToken(res.loginToken);
+					} else {
+						// 暂无抽奖资格
+						this.setState({
+							showLoginTip: true
+						});
+					}
+				},
+				(err) => {
+					this.props.toast.info(err.msgInfo);
+				}
+			);
+	};
+
 	// 获取我的奖品
 	getMyAward = () => {
-		token = Cookie.get('fin-v-card-token');
-		if (!token) {
-			this.setState({
-				type: 'alert_tel'
-			});
-			return;
-		}
-		this.getrecordForUser('01');
+		this.isAuthFunc(() => {
+			this.getrecordForUser('01');
+		});
 	};
 	// 用户中奖列表
 	getrecordForUser = (type) => {
@@ -186,7 +311,10 @@ export default class dazhuanpan_page extends PureComponent {
 		this.props.$fetch.post(API.awardRecords, params).then((res) => {
 			if (res.msgCode === 'PTM0000') {
 				this.setState({
-					allUsersAward: (res.data && res.data.data) || []
+					allUsersAward: (res.data && res.data.data) || [{
+                        mblHid:'185****4323',
+                        valDes:'5元优惠券'
+                    }]
 				});
 			} else {
 				Toast.info(res.msgInfo);
@@ -257,13 +385,6 @@ export default class dazhuanpan_page extends PureComponent {
 
 	// 转盘按钮
 	onloadZhuan = async () => {
-		token = Cookie.get('fin-v-card-token');
-		if (!token) {
-			this.setState({
-				type: 'alert_tel'
-			});
-			return;
-		}
 		if (store.getRewardCount() && Number(store.getRewardCount()) <= 0) {
 			this.setState({
 				count: '0',
@@ -272,7 +393,9 @@ export default class dazhuanpan_page extends PureComponent {
 			return;
 		}
 		if (this.isRotated) return; //如果正在旋转退出程序
-		this.getCount();
+		this.isAuthFunc(() => {
+			this.getCount();
+		});
 	};
 
 	setalertType = (type) => {
@@ -296,6 +419,26 @@ export default class dazhuanpan_page extends PureComponent {
 						<img src={this.state.codeInfo !== 'PCC-MARKET-0001' ? notstart : over} />{' '}
 					</div>
 				) : null}
+				{this.state.showLoginTip && (
+					<div className={styles.modal}>
+						<div className={styles.mask} />
+						<div className={[ styles.modalWrapper, styles.tipWrapper ].join(' ')}>
+							<div className={styles.tipText}>
+								<span>小主～</span>
+								<br />
+								<span>先去登录才能参与活动哦～</span>
+							</div>
+							<div
+								className={styles.closeBtn}
+								onClick={() => {
+									this.setState({
+										showLoginTip: false
+									});
+								}}
+							/>
+						</div>
+					</div>
+				)}
 				{!this.state.codeInfo ? (
 					<div>
 						<LoginAlert
