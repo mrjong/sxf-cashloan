3a1144637f7cd314dfad2a7022016347dbddbca8
chenjiong <kapp1e@163.com>
2019-03-23 17:19:00 +0800

    ..

--- src/pages/home/essential_information_page/index.js
+++ src/pages/home/essential_information_page/index.js
@@ -64,9 +64,9 @@ export default class essential_information_page extends PureComponent {
 
 	componentDidMount() {
 		// 安卓键盘抬起会触发resize事件，ios则不会
-		window.addEventListener('resize', function() {
+		window.addEventListener('resize', function () {
 			if (document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') {
-				window.setTimeout(function() {
+				window.setTimeout(function () {
 					document.activeElement.scrollIntoViewIfNeeded();
 				}, 0);
 			}
@@ -75,9 +75,9 @@ export default class essential_information_page extends PureComponent {
 
 	componentWillUnmount() {
 		buryingPoints();
-		window.removeEventListener('resize', function() {
+		window.removeEventListener('resize', function () {
 			if (document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') {
-				window.setTimeout(function() {
+				window.setTimeout(function () {
 					document.activeElement.scrollIntoViewIfNeeded();
 				}, 0);
 			}
@@ -101,14 +101,14 @@ export default class essential_information_page extends PureComponent {
 					this.setState({
 						relatValue:
 							res.data && res.data.cntRelTyp1
-								? [ `${res.data.cntRelTyp1}` ]
+								? [`${res.data.cntRelTyp1}`]
 								: store.getRelationValue() ? store.getRelationValue() : []
 					});
 				} else {
 					this.props.toast.info(res.msgInfo);
 				}
 			},
-			(error) => {}
+			(error) => { }
 		);
 	};
 
@@ -132,22 +132,22 @@ export default class essential_information_page extends PureComponent {
 		let cityPattern = new RegExp(`^[\\u4E00-\\u9FA5]*${city}[a-zA-Z0-9\\u4E00-\\u9FA5]*$`);
 		this.props.$fetch.get(`${API.getProv}`).then((result) => {
 			if (result && result.data) {
-				const provItem = reducedFilter(result.data, [ 'key', 'value' ], (item) => {
+				const provItem = reducedFilter(result.data, ['key', 'value'], (item) => {
 					let proPattern2 = new RegExp(`^[\\u4E00-\\u9FA5]*${item.value}[a-zA-Z0-9\\u4E00-\\u9FA5]*$`);
 					if (proPattern.test(item.value) || proPattern2.test(pro)) {
 						return item;
 					}
 				});
 				this.props.$fetch.get(`${API.qryCity}/${provItem[0].key}`).then((result2) => {
-					const cityItem = reducedFilter(result2.data, [ 'key', 'value' ], (item2) => {
+					const cityItem = reducedFilter(result2.data, ['key', 'value'], (item2) => {
 						let cityPattern2 = new RegExp(`^[\\u4E00-\\u9FA5]*${item2.value}[a-zA-Z0-9\\u4E00-\\u9FA5]*$`);
 						if (cityPattern.test(item2.value) || cityPattern2.test(city)) {
 							return item2;
 						}
 					});
 					this.setState({
-						provValue: provItem && cityItem && [ provItem[0].key + '', cityItem[0].key + '' ],
-						provLabel: provItem && cityItem && [ provItem[0].value + '', cityItem[0].value + '' ]
+						provValue: provItem && cityItem && [provItem[0].key + '', cityItem[0].key + ''],
+						provLabel: provItem && cityItem && [provItem[0].value + '', cityItem[0].value + '']
 					});
 					console.log(this.state.provValue, this.state.provLabel);
 				});
@@ -306,7 +306,7 @@ export default class essential_information_page extends PureComponent {
 				<div className={style.labelDiv}>
 					{getFieldDecorator('city', {
 						initialValue: this.state.provValue,
-						rules: [ { required: true, message: '请选择城市' } ],
+						rules: [{ required: true, message: '请选择城市' }],
 						onChange: (value, label) => {
 							this.selectSure({
 								value: JSON.stringify(value),
@@ -350,7 +350,7 @@ export default class essential_information_page extends PureComponent {
 				</div>
 				<div className={`${style.inputDiv}`} style={{ marginTop: 0 }}>
 					{getFieldDecorator('address', {
-						rules: [ { required: true, message: '请输入常住地址' }, { validator: this.validateAddress } ],
+						rules: [{ required: true, message: '请输入常住地址' }, { validator: this.validateAddress }],
 						onChange: (value) => {
 							// 本地缓存常住地址
 							store.setAddress(value);
@@ -371,14 +371,14 @@ export default class essential_information_page extends PureComponent {
 						</InputItem>
 					)}
 				</div>
-                <div className={style.mt50}></div>
+				<div className={style.mt50}></div>
 				<div className={style.infromationTitle}>
 					<span>亲属联系人信息</span>
 				</div>
 				<div className={style.labelDiv}>
 					{getFieldDecorator('cntRelTyp1', {
 						initialValue: this.state.relatValue,
-						rules: [ { required: true, message: '请选择联系人关系' } ],
+						rules: [{ required: true, message: '请选择联系人关系' }],
 						onChange: (value, label) => {
 							store.setRelationValue(value);
 							this.selectSure({
@@ -413,7 +413,7 @@ export default class essential_information_page extends PureComponent {
 				</div>
 				<div className={style.labelDiv} style={{ marginTop: 0 }}>
 					{getFieldDecorator('linkman', {
-						rules: [ { required: true, message: '请输入联系人姓名' }, { validator: this.validateName } ],
+						rules: [{ required: true, message: '请输入联系人姓名' }, { validator: this.validateName }],
 						onChange: (value) => {
 							store.setLinkman(value);
 							this.setState({ linkman: value });
@@ -435,7 +435,7 @@ export default class essential_information_page extends PureComponent {
 				</div>
 				<div className={`${style.labelDiv}`} style={{ marginTop: 0 }}>
 					{getFieldDecorator('linkphone', {
-						rules: [ { required: true, message: '请输入联系人手机号' }, { validator: this.validatePhone } ],
+						rules: [{ required: true, message: '请输入联系人手机号' }, { validator: this.validatePhone }],
 						onChange: (value) => {
 							store.setLinkphone(value);
 							this.setState({ linkphone: value });
@@ -461,8 +461,9 @@ export default class essential_information_page extends PureComponent {
 					<p className={style.desOne}>*输入有效的紧急联系人信息，有利于提升您的审批额度，我们将对您提供的信息严格保密</p>
 				</div>
 				<ButtonCustom onClick={this.handleSubmit} className={style.sureBtn}>
-					完成
+					{needNextUrl ? '下一步' : '完成'}
 				</ButtonCustom>
+        <p className="bottomTip">怕逾期，用还到</p>
 			</div>
 		);
 	}
