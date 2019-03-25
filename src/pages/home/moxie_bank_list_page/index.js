6cb01663beff1ec7d1942e2caa18aa9e42e30111
chenjiong <kapp1e@163.com>
2019-03-25 11:50:03 +0800

    ...

--- src/pages/home/moxie_bank_list_page/index.js
+++ src/pages/home/moxie_bank_list_page/index.js
@@ -24,7 +24,7 @@ export default class moxie_bank_list_page extends Component {
 		isnoData: false
 	};
 	componentWillMount() {
-        // 信用卡直接返回的问题
+		// 信用卡直接返回的问题
 		// store.removeBankMoxie();
 		this.mxoieCardList();
 		this.getBackUrl();
@@ -114,13 +114,13 @@ export default class moxie_bank_list_page extends Component {
 		}
 		buriedPointEvent(moxie_bank_list.bankChooes, {
 			bankName: item.name
-        });
-        // 信用卡直接返回的问题
-        store.setBankMoxie(true);
-        store.setGoMoxie(true)
+		});
+		// 信用卡直接返回的问题
+		store.setBankMoxie(true);
+		store.setGoMoxie(true)
 
-        window.location.href = item.href + '&showTitleBar=NO'
-        // window.history.pushState(null, null, item.href + '&showTitleBar=NO');
+		window.location.href = item.href + '&showTitleBar=NO'
+		// window.history.pushState(null, null, item.href + '&showTitleBar=NO');
 		// location.href = item.href + '&showTitleBar=NO';
 	};
 	// 重新加载
@@ -135,30 +135,33 @@ export default class moxie_bank_list_page extends Component {
 				{needNextUrl ? (
 					<StepBar current={3} />
 				) : (
-					<div>
-						<div className={style.title}>
-							选择发卡银行
+						<div className={style.moxie_bank_top}>
+							<div className={style.title}>
+								选择发卡银行
 							<span className={style.subTitle}>获3项优质服务</span>
+							</div>
+							<div className={style.bankDesc}>
+								<span>
+									<i className={style.dot} />
+									高效管理信用卡
+							</span>
+								<span>
+									<i className={style.dot} />
+									一键同步账单
+							</span>
+								<span>
+									<i className={style.dot} />
+									多重加密
+							</span>
+							</div>
 						</div>
-						<div className={style.bankDesc}>
-							<span>
-								<i className={style.dot} />
-								高效管理信用卡
-							</span>
-							<span>
-								<i className={style.dot} />
-								一键同步账单
-							</span>
-							<span>
-								<i className={style.dot} />
-								多重加密
-							</span>
-						</div>
-					</div>
-				)}
+					)}
 
 				{this.state.bankList && this.state.bankList.length > 0 ? (
 					<div>
+						<div className={style.infromationTitle}>
+							<span>通过网银添加账单</span>
+						</div>
 						<div className={style.bankList}>
 							{this.state.bankList.map((item, index) => {
 								// if (index <= this.state.lengthNum) {
