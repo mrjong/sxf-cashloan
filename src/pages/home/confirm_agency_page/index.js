a9655b4815b1a7ff540941d925cc9571c359cac7
孙建坤 <sun_jk@suixingpay.com>
2019-03-26 14:07:28 +0800

    修改后台接口改动

--- src/pages/home/confirm_agency_page/index.js
+++ src/pages/home/confirm_agency_page/index.js
@@ -243,7 +243,7 @@ export default class confirm_agency_page extends PureComponent {
     const { lendersDate, repaymentDate, cardBillAmt, repayInfo } = this.state;
     this.props.$fetch.post(`${API.queryFundInfo}`, {
       loanAmount: cardBillAmt,
-      periodCount: repaymentDate.periodUnit === 'D' ? repaymentDate.periodLth : repaymentDate.periodCount,
+      periodCount: repaymentDate.periodCount,
       periodUnit: repaymentDate.periodUnit,
       agrNo: repayInfo.withDrawAgrNo,
       wtdwTyp: lendersDate.value,
