e357b55474b667caac305d3c5466f4d44e4364ea
shawn2016 <787990963@qq.com>
2019-04-16 16:23:30 +0800

    modal组件

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Modal } from 'antd-mobile';
 import styles from './index.scss';
 import huodongTootip3 from '../../assets/images/home/huodongTootip3.png';
 import huodongTootip1 from '../../assets/images/home/huodongTootip1.png';
@@ -8,44 +9,55 @@ import mianxi30 from './img/mianxi30.png';
 class ActivityModal extends React.Component {
 	constructor(props) {
 		super(props);
-	}
+		this.state = {
+			isShowActivityModal: false
+		};
+    }
+    componentWillMount() {
+        setTimeout(() => {
+          this.setState({
+            isShowActivityModal: true
+          });
+        }, 300);
+      }
 	// 优先弹688  再弹出免息  再弹iphone
 	render() {
 		const { closeActivityModal, history, modalType, activityModalBtn } = this.props;
 		return (
-			<div className={styles.modal}>
-				<div className={styles.mask} />
-				<div className={styles.modalWrapper}>
-					<div className={styles.content}>
-						{/* 大图 */}
-						{modalType === 'huodongTootip1' ? <img src={huodongTootip1} /> : null}
-						{modalType === 'huodongTootip3' ? <img src={huodongTootip3} /> : null}
-						{modalType === 'mianxi30' ? <img src={mianxi30} className={styles.mianxi30} /> : null}
-						{/* 按钮 */}
-						{modalType === 'huodongTootip3' ? (
-							<img
-								className={styles.huodongTootipBtn}
-								src={huodongTootipBtn3}
-								onClick={() => {
-									activityModalBtn('huodongTootip3');
-								}}
-							/>
-						) : null}
-						{modalType === 'mianxi30' ? (
-							<div
-								className={styles.btn_mianxi}
-								onClick={() => {
-									activityModalBtn('mianxi30');
-								}}
-							>
-								立即参与
-							</div>
-						) : null}
+			<Modal  wrapClassName="home_modal_warp" visible={this.state.isShowActivityModal} transparent>
+				<div className={styles.modal}>
+					<div className={styles.modalWrapper}>
+						<div className={styles.content}>
+							{/* 大图 */}
+							{modalType === 'huodongTootip1' ? <img src={huodongTootip1} /> : null}
+							{modalType === 'huodongTootip3' ? <img src={huodongTootip3} /> : null}
+							{modalType === 'mianxi30' ? <img src={mianxi30} className={styles.mianxi30} /> : null}
+							{/* 按钮 */}
+							{modalType === 'huodongTootip3' ? (
+								<img
+									className={styles.huodongTootipBtn}
+									src={huodongTootipBtn3}
+									onClick={() => {
+										activityModalBtn('huodongTootip3');
+									}}
+								/>
+							) : null}
+							{modalType === 'mianxi30' ? (
+								<div
+									className={styles.btn_mianxi}
+									onClick={() => {
+										activityModalBtn('mianxi30');
+									}}
+								>
+									立即参与
+								</div>
+							) : null}
+						</div>
+						{/* 关闭按钮 */}
+						<div className={styles.closeBtn} onClick={closeActivityModal} />
 					</div>
-					{/* 关闭按钮 */}
-					<div className={styles.closeBtn} onClick={closeActivityModal} />
 				</div>
-			</div>
+			</Modal>
 		);
 	}
 }
