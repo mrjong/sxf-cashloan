ff470c269f48490daf332c5b213c931ca795045d
孙建坤 <sun_jk@suixingpay.com>
2019-04-16 19:46:50 +0800

    修改逾期进度页面

--- src/pages/order/overdue_progress_page/index.js
+++ src/pages/order/overdue_progress_page/index.js
@@ -2,9 +2,7 @@ import React, { PureComponent } from 'react';
 import ButtonCustom from 'components/ButtonCustom';
 import fetch from "sx-fetch";
 import { store } from 'utils/store';
-import { buriedPointEvent } from 'utils/analytins';
-import { order } from 'utils/analytinsType';
-// import 'utils/noRouterBack'
+import { setBackGround } from 'utils/background';
 import styles from './index.scss';
 
 const API = {
@@ -17,12 +15,68 @@ const API = {
   payFrontBack: '/bill/payFrontBack', // 用户还款新接口
 }
 @fetch.inject()
+@setBackGround('#FFF')
 
 export default class overdue_progress_page extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      
+      progressList: [
+        {
+          date: '2019-04-05',
+          time: '11:43:12',
+          content: '账单已逾期',
+          isActive: true,
+        },
+        {
+          date: '2019-04-05',
+          time: '11:43:12',
+          content: '逾期记录即将上报百行征信',
+          isActive: true,
+        },
+        {
+          date: '2019-04-05',
+          time: '11:43:12',
+          content: '逾期记录已经上报百行征信',
+          isActive: true,
+        },
+        {
+          date: '2019-04-05',
+          time: '11:43:12',
+          content: '逾期记录申请上报人行征信',
+          isActive: true,
+        },
+        {
+          date: '2019-04-05',
+          time: '',
+          content: '即将向仲裁委申请仲裁',
+          isActive: false,
+        },
+        {
+          date: '2019-04-05',
+          time: '',
+          content: '已申请仲裁处理',
+          isActive: false,
+        },
+        {
+          date: '2019-04-05',
+          time: '',
+          content: '仲裁申请受理中',
+          isActive: false,
+        },
+        {
+          date: '2019-04-05',
+          time: '',
+          content: '裁决书已下发，请尽快下载',
+          isActive: false,
+        },
+        {
+          date: '2019-04-05',
+          time: '',
+          content: '仲裁执行中',
+          isActive: false,
+        },
+      ]
     }
   }
   componentWillMount() {
@@ -35,16 +89,40 @@ export default class overdue_progress_page extends PureComponent {
     }
   }
 
-  // 返回首页
-  backHome = () => {
+  // 返回账单详情
+  backOrderDtl = () => {
     this.props.history.goBack()
   }
 
   render() {
-    const { isShowTipsModal } = this.state;
+    const { progressList } = this.state;
     return (
       <div className={styles.overdue_progress_page}>
-        
+        {
+          progressList && progressList.length ?
+          <ul className={styles.progress_list}>
+            {
+              progressList.map((item, index) => {
+                return (
+                  <li key={index} className={item.isActive ? styles.active : null}>
+                    <p className={styles.dateTimeBox}>
+                      <span className={styles.dateBox}>{item.date}</span>
+                      <span>{item.time}</span>
+                    </p>
+                    <p className={styles.desc}>{item.content}</p>
+                    <i className={styles.arrow} />
+                    <i className={styles.stepItem} />
+                    {
+                      index !== 0 && <i className={styles.lineItem} />
+                    }
+                  </li>
+                )
+              })
+            }
+          </ul>
+          : null
+        }
+        <ButtonCustom onClick={this.backOrderDtl} className={styles.back_btn}>立即还款</ButtonCustom>        
       </div>
     )
   }
