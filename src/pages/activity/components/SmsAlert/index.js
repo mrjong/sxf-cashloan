0522817f6b3e96b4f137d0cefd6f97769e1a43ce
孙建坤 <sun_jk@suixingpay.com>
2019-05-09 16:48:23 +0800

    去掉调试代码

--- src/pages/activity/components/SmsAlert/index.js
+++ src/pages/activity/components/SmsAlert/index.js
@@ -426,7 +426,6 @@ export default class SmsAlert extends Component {
 		const { modalBtnBuryPoint } = this.props;
 		const { authToken, otherProps_type } = this.state;
 		modalBtnBuryPoint(); // 埋点需求
-		return;
 		this.props.form.validateFields((err, values) => {
 			if (err && err.smsCd) {
 				delete err.smsCd;
