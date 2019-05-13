580e83090afb5e28d6b3c32d40ecc82486ffcebf
孙建坤 <sun_jk@suixingpay.com>
2019-05-13 15:30:26 +0800

    修改短验可以输入的bug，并且校验错误

--- src/pages/activity/components/SmsAlert/index.js
+++ src/pages/activity/components/SmsAlert/index.js
@@ -314,6 +314,7 @@ export default class SmsAlert extends Component {
 							otherProps_type,
 							loginProps_needLogin_copy: false,
 							isShowMobile: false,
+							disabled: true,
 						});
 						this.props.form.setFieldsValue({
 							phoneValue: res.mblNoHid,
