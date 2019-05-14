a4fe86638493fd0eb94f2b1d1952d00b85ba0fa6
shawn2016 <787990963@qq.com>
2019-05-14 11:59:01 +0800

    下线没有用的活动

--- src/pages/activity/router.js
+++ src/pages/activity/router.js
@@ -5,52 +5,52 @@ export default [
 		arrowHide: true,
 		component: () => import('pages/activity/dazhuanpan_page')
 	},
-	{
-		path: '/activity/wxshare_page',
-		title: '微信分享',
-		arrowHide: 'empty',
-		headerHide: true,
-		component: () => import('pages/activity/wxshare_page')
-	},
-	{
-		path: '/activity/newuser_page',
-		title: '拉新活动',
-		arrowHide: 'empty',
-		headerHide: true,
-		component: () => import('pages/activity/newuser_page')
-	},
-	{
-		path: '/activity/coupon_page',
-		title: '百万红包天天抢',
-		arrowHide: 'empty',
-		component: () => import('pages/activity/coupon_page')
-	},
-	{
-		path: '/activity/dazhuanpan_316_page',
-		title: '幸运大转盘',
-		arrowHide: 'empty',
-		component: () => import('pages/activity/dazhuanpan_316_page')
-	},
+	// {
+	// 	path: '/activity/wxshare_page',
+	// 	title: '微信分享',
+	// 	arrowHide: 'empty',
+	// 	headerHide: true,
+	// 	component: () => import('pages/activity/wxshare_page')
+	// },
+	// {
+	// 	path: '/activity/newuser_page',
+	// 	title: '拉新活动',
+	// 	arrowHide: 'empty',
+	// 	headerHide: true,
+	// 	component: () => import('pages/activity/newuser_page')
+	// },
+	// {
+	// 	path: '/activity/coupon_page',
+	// 	title: '百万红包天天抢',
+	// 	arrowHide: 'empty',
+	// 	component: () => import('pages/activity/coupon_page')
+	// },
+	// {
+	// 	path: '/activity/dazhuanpan_316_page',
+	// 	title: '幸运大转盘',
+	// 	arrowHide: 'empty',
+	// 	component: () => import('pages/activity/dazhuanpan_316_page')
+	// },
 	{
 		path: '/activity/jupei_page',
 		title: '还到拒就赔',
 		arrowHide: 'empty',
 		component: () => import('pages/activity/jupei_page')
 	},
-	{
-		path: '/activity/funsisong_page',
-		title: '全民FUN肆送',
-		arrowHide: 'empty',
-		component: () => import('pages/activity/funsisong_page')
-	},
+	// {
+	// 	path: '/activity/funsisong_page',
+	// 	title: '全民FUN肆送',
+	// 	arrowHide: 'empty',
+	// 	component: () => import('pages/activity/funsisong_page')
+	// },
 	// {
 	// 	path: '/activity/mianxi418_page',
 	// 	title: '最高免息30天',
 	// 	component: () => import('pages/activity/mianxi418_page')
 	// },
-	{
-		path: '/activity/wenjuan_page',
-		title: '答题抽奖',
-		component: () => import('pages/activity/wenjuan_page')
-	}
+	// {
+	// 	path: '/activity/wenjuan_page',
+	// 	title: '答题抽奖',
+	// 	component: () => import('pages/activity/wenjuan_page')
+	// }
 ];
