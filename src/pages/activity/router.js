9f50113ef450ba6c1ee2c1a8f0b2e17ac24cca41
shawn2016 <787990963@qq.com>
2019-05-14 12:00:13 +0800

    下线没有用的活动

--- src/pages/activity/router.js
+++ src/pages/activity/router.js
@@ -1,10 +1,10 @@
 export default [
-	{
-		path: '/activity/dazhuanpan_page',
-		title: '大转盘',
-		arrowHide: true,
-		component: () => import('pages/activity/dazhuanpan_page')
-	},
+	// {
+	// 	path: '/activity/dazhuanpan_page',
+	// 	title: '大转盘',
+	// 	arrowHide: true,
+	// 	component: () => import('pages/activity/dazhuanpan_page')
+	// },
 	// {
 	// 	path: '/activity/wxshare_page',
 	// 	title: '微信分享',
