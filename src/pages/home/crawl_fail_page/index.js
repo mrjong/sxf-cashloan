162237e7a0c80009d0548bd12d9b03131e742ee0
yang_fy <yang_fy@suixingpay.com>
2019-05-17 19:43:19 +0800

    选择银行卡   进度页面

---
+++ src/pages/home/crawl_fail_page/index.js
@@ -0,0 +1,73 @@
+import React, { PureComponent } from 'react';
+import style from './index.scss';
+import fetch from 'sx-fetch';
+import { setBackGround } from 'utils/background';
+import { store } from 'utils/store';
+import qs from 'qs';
+import { Popover} from 'antd-mobile';
+import { buriedPointEvent } from 'utils/analytins';
+import { home } from 'utils/analytinsType';
+import progressIcon from  './img/crawl.png'
+const API = {
+
+};
+
+const Item = Popover.Item;
+
+@fetch.inject()
+@setBackGround('#fff')
+export default class crawl_progress_page extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+  render() {
+    let {  } = this.state
+    return (
+      <div className={style.crawl_fail_page}>
+        <img src={progressIcon} className={style.progress_icon}/>
+        <div className={style.progress_desc}>
+          资料收齐，只差添加需要还款的信用卡，
+          <span>已完成98% </span>
+          <span className={style.apply_success}> 申请成功</span>
+        </div>
+        <Popover
+           visible='true'
+           placement='bottom'
+           className={style.aa}
+           overlay={[
+             (<Item
+               key="4"
+               value="scan"
+               data-seed="logId"
+               disabled='true'
+               style={{color: '#fff', fontSize: '0.28rem', opacity: 0.9}}>
+               多次失败建议换张信用卡，已送您一张免息券
+             </Item>),
+           ]}
+           onSelect={this.onSelect}
+        >
+          <div className={style.popover_inner}>信用卡账单导入失败</div>
+        </Popover>
+        <div className={style.button}>选择导入其他银行卡</div>
+        <div style={{textAlign: 'center'}} onClick={()=>{
+          this.props.history.replace('/home/crawl_progress_page')
+        }}>尝试再次导入</div>
+      </div>
+    );
+  }
+}
--- src/pages/mine/credit_list_page/index.js
+++ src/pages/mine/credit_list_page/index.js
@@ -4,7 +4,8 @@ import fetch from 'sx-fetch';
 import qs from 'qs';
 import styles from './index.scss';
 import { Icon } from 'antd-mobile';
-
+import select from './img/select.png'
+import not_select from './img/not_select.png'
 const API = {
   CREDCARDLIST: '/index/usrCredCardList', // 银行卡列表
   CARDAUTH: '/auth/cardAuth', // 0404-信用卡授信
@@ -72,7 +73,7 @@ export default class credit_list_page extends PureComponent {
       // bankCode: obj.bankCode,
       autId: obj.autId,
     });
-    this.sendSelectedCard(obj.autId, true);
+    // this.sendSelectedCard(obj.autId, true);
     // this.props.$fetch.get(`/index/cacheCredCard/${obj.autId}`).then(() => {
     //   this.props.history.replace(backUrlData);
     // })
@@ -86,6 +87,10 @@ export default class credit_list_page extends PureComponent {
   };
   // 告诉后台选中的是哪张卡
   sendSelectedCard = (autId, jumpFlag) => {
+    if(!autId){
+      this.props.toast.info('请选择银行卡')
+      return
+    }
     this.props.$fetch.get(`${API.CACHECREDCARD}/${autId}`).then(
       res => {
         if (res.msgCode === "PTM0000") {
@@ -113,17 +118,18 @@ export default class credit_list_page extends PureComponent {
         } else {
           store.setMoxieBackUrl('/mine/credit_list_page');
         }
-      
+
 		this.props.history.push({ pathname: '/home/moxie_bank_list_page' });
 	};
 
   render() {
+    let {autId} = this.state
     return (
       <div className={styles.credit_list_page}>
         {
           this.state.cardList.length ?
             <div>
-              <p className={styles.card_tit}>已绑定信用卡</p>
+              <p className={styles.card_tit}>选择你想还款信用卡</p>
               <ul className={styles.card_list}>
                 {
                   this.state.cardList.map((item, index) => {
@@ -131,13 +137,13 @@ export default class credit_list_page extends PureComponent {
                     const icoClass = item.autSts === '2' ? `bank_ico bank_ico_${item.bankNo}` : `bank_ico black_logo`;
                     return (
                       <li
-                        className={isSelected ? styles.active : ''}
+                        className={`${isSelected ? styles.active : ''} ${styles.bank_item}` }
                         key={index}
                         onClick={
                           this.selectCard.bind(this, item)
                         }
                       >
-                        <span className={icoClass}></span>
+                        <span className={`${icoClass} ${styles.bank_icon}`} ></span>
                         {
                           item.autSts === '1' ?
                             <span className={`${styles.bank_name} ${styles.pending}`}>审核中 ····</span>
@@ -146,15 +152,25 @@ export default class credit_list_page extends PureComponent {
                               :
                               <span className={styles.bank_name}>{item.bankName}</span>
                         }
+                        <div className={styles.surplus_desc}>信用卡剩余应还金额(元)</div>
+                        <div className={styles.bill_remain_amt}>{item.billRemainAmt || item.cardBillAmt}</div>
                         {
                           item.autSts === '2' ?
-                            <span>···· {item.last}</span>
+                            <span>
+                              <span className={styles.bank_number}>
+                                <span style={{marginRight: '.3rem'}}>****</span>
+                                <span style={{marginRight: '.3rem'}}>****</span>
+                                <span style={{marginRight: '.3rem'}}>****</span>
+                                {item.last}
+                              </span>
+                              <span className={styles.bank_date}>还款日：{item.cardBillDt}</span>
+                            </span>
                             : null
                         }
                         {
                           isSelected ? (
-                            <Icon type="check-circle-o" color='#5CE492' className={styles.selected_ico}/>
-                          ) : null
+                            <img src={select} className={styles.select_icon}/>
+                          ) : <img src={not_select} className={styles.select_icon}/>
                         }
                       </li>
                     )
@@ -164,7 +180,8 @@ export default class credit_list_page extends PureComponent {
             </div>
             : null
         }
-        <p onClick={this.goToNewMoXie} className={styles.add_card}><i className={styles.add_ico}></i>新增授权卡</p>
+        <div className={styles.button} onClick={()=>{this.sendSelectedCard(autId, true)}}>确认</div>
+        <p onClick={this.goToNewMoXie} className={styles.add_card}>新增需要导入账单的信用卡</p>
       </div>
     )
   }
