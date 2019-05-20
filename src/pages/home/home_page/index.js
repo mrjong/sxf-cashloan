c077c0da1b885d466ccac4e47af04d7e03cdb85a
shawn2016 <787990963@qq.com>
2019-05-20 18:25:35 +0800

    ==

--- src/pages/home/home_page/index.js
+++ src/pages/home/home_page/index.js
@@ -638,25 +638,6 @@ export default class home_page extends PureComponent {
 		}
 	};
 
-	showCreditModal = () => {
-		const { usrIndexInfo } = this.state;
-		const { cardBillSts } = usrIndexInfo.indexData;
-		if (cardBillSts === '00') {
-			this.props.toast.info('还款日已到期，请更新账单获取最新账单信息');
-			return;
-		} else if (cardBillSts === '02') {
-			this.props.toast.info('已产生新账单，请更新账单或代偿其他信用卡', 2, () => {
-				// 跳新版魔蝎
-				store.setMoxieBackUrl('/home/crawl_progress_page');
-				this.props.history.push({ pathname: '/home/moxie_bank_list_page' });
-			});
-			return;
-		}
-		this.setState({
-			isShowCreditModal: true
-		});
-	};
-
 	// 神策用户绑定
 	queryUsrSCOpenId = () => {
 		if (!store.getQueryUsrSCOpenId()) {
