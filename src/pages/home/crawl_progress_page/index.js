933f5da75bcdd83a8bc4dfb3b3e8b1256f3c862c
yang_fy <yang_fy@suixingpay.com>
2019-05-20 16:41:46 +0800

    选择银行卡   进度页面

--- src/pages/home/crawl_progress_page/index.js
+++ src/pages/home/crawl_progress_page/index.js
@@ -8,39 +8,35 @@ import qs from 'qs';
 import { buriedPointEvent } from 'utils/analytins';
 import { home } from 'utils/analytinsType';
 const API = {
-  USR_INDEX_INFO: '/index/usrIndexInfo', // 0103-首页信息查询接口
-  USER_IMPORT: '/auth/cardAuth'
+  CHECK_CARD_AUTH: '/auth/checkCardAuth/', // 0103-首页信息查询接口
+  USER_IMPORT: '/auth/cardAuth',
+	CRED_CARD_COUNT: '/index/usrCredCardCount', // 授信信用卡数量查询
 };
-let timerPercent = null;
-let timer = null;
-let data = [
-  { desc: '正在建立安全链接', status: '连接中', percent: 24, speed: 200},
-  { desc: '正在登陆银行', status: '登录中', percent: 49, speed: 400 },
-  { desc: '正在获取银行卡信息', status: '获取中', percent: 74, speed: 800},
-  { desc: '正在分析账单流水', status: '处理中', percent: 99, speed: 0},
-]
+let timerPercent ;
+let timer ;
+let data ;
 @fetch.inject()
 @setBackGround('#fff')
 export default class crawl_progress_page extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      percent: 0,
-      percentArray: [
-        {percent: 24, speed: 200},
-        {percent: 49, speed: 400},
-        {percent: 74, speed: 800},
-        {percent: 99, speed: 0}
-      ],
-      time: 0,
-      isFail: false,
-      apiStatus: false,
-      usrIndexInfo: {}
+      percent: 0,   //百分比
+      time: 0,    //时间分秒
+      isFail: false,   //失败显露标志
+      autId : store.getAutId ? store.getAutId() : ''
     };
   }
 
   componentWillMount() {
-
+    timerPercent = null;
+    timer = null;
+    data =  [
+      { desc: '正在建立安全链接', status: '连接中', percent: 24, speed: 200},
+      { desc: '正在登陆银行', status: '登录中', percent: 49, speed: 400 },
+      { desc: '正在获取银行卡信息', status: '获取中', percent: 74, speed: 800},
+      { desc: '正在分析账单流水', status: '处理中', percent: 98, speed: 0},
+    ]
   }
 
   componentDidMount() {
@@ -48,7 +44,6 @@ export default class crawl_progress_page extends PureComponent {
     this.goTime()
     this.queryUsrInfo()
   }
-
   goTime(){
     timer = setInterval(()=>{
       this.setState({
@@ -57,82 +52,83 @@ export default class crawl_progress_page extends PureComponent {
         if(this.state.time> 29){
           clearInterval(timerPercent)
           clearInterval(timer)
-          if(!this.state.apiStatus){
-            this.setState({ isFail: true })
-            data[3].status = '失败'
-          }
+          this.setState({ isFail: true })
+          data[3].status = '失败'
         } else if (this.state.time % 5 === 0) {
           this.queryUsrInfo()
         }
       })
     }, 1000)
   }
-  goProgress(number) {
+  goProgress(number, cb) {
     timerPercent = setInterval(()=>{
       let { percent } = this.state
       this.setState({
-        percent: percent + 1,
+        percent: percent ===100 ? percent : percent + 1,
       },()=>{
         if(number===10){
-          this.successGetBankInfo()
+          data.forEach(item=>{
+            if(percent === item.percent) item.status = '完成'
+          })
+          if(percent > 99) {
+            clearInterval(timerPercent)
+            setTimeout(()=>{ cb() }, 300)
+          }
           return
         }
         data.forEach(item=>{
-          if(percent=== item.percent){
-            clearInterval(timerPercent)
-            item.status = '完成'
-            item.speed !== 0 && this.goProgress(item.speed)
+          if(percent=== item.percent){ clearInterval(timerPercent)
+            item.status = '完成' ; item.speed !== 0 && this.goProgress(item.speed)
           }
         })
       })
     }, number)
   }
-  successGetBankInfo(){
-    let { percent } = this.state
-    data.forEach(item=>{
-      if(percent === item.percent){
-        item.status = '完成'
-      }
-    })
-    if(percent > 99){
-      clearInterval(timerPercent)
-      setTimeout(()=>{this.props.history.replace('/mine/credit_list_page')}, 300)
-    }
-  }
   //查询用户相关信息
   queryUsrInfo = () => {
+    let {autId} = this.state
     this.props.$fetch
-      .post(API.USR_INDEX_INFO)
+      .get(API.CHECK_CARD_AUTH+ autId)
       .then((res) => {
-        this.setState(
-          {
-            usrIndexInfo: res.data.indexData ? res.data : Object.assign({}, res.data, { indexData: {} })
-          },
-          () => {
-            const { indexSts, indexData } = this.state.usrIndexInfo;
-            if (indexSts === 'LN0002' || (indexSts === 'LN0003' && indexData.autSts === '1')) {
-              //更新中
-              return false
-            } else if (indexSts === 'LN0010' || (indexSts === 'LN0003' && indexData.autSts === '3')) {
-              //更新失败
-              return false
-            } else if (indexSts === 'LN0003' && indexData.autSts === '2') {
-              //更新成功
-              this.setState({apiStatus: true},()=>{
-                clearInterval(timerPercent)
-                clearInterval(timer)
-                this.goProgress(10)
-              })
-            }
+        if (res.msgCode === "PTM0000") {
+          if (res.data === '02') {
+            clearInterval(timerPercent)
+            clearInterval(timer)
+            this.goProgress(10,()=>{
+              this.requestCredCardCount()
+            })
+          } else if(res.jumpFlag === '03'){
+            this.props.history.replace('/home/crawl_fail_page')
           }
-        );
+        } else {
+          res.msgInfo && this.props.toast.info(res.msgInfo)
+        }
+      }
+    ).catch((err) => {
+      this.state.retryCount--;
+      this.setState({
+        showAgainUpdateBtn: true
+      });
+    });
+  };
+
+  requestCredCardCount = () => {
+    let {autId} = this.state
+    this.props.$fetch
+      .post(API.CRED_CARD_COUNT)
+      .then((result) => {
+        if (result && result.msgCode === 'PTM0000') {
+          if(result.data.count> 1){
+            this.props.history.replace(`/mine/credit_list_page?autId=${autId}`)
+          } else {
+            this.props.history.replace('/home/loan_repay_confirm_page')
+          }
+        } else {
+          this.props.toast.info(result.msgInfo);
+        }
       })
       .catch((err) => {
-        this.hideProgress();
-        this.state.retryCount--;
-        this.setState({
-          showAgainUpdateBtn: true
-        });
+        this.props.toast.info(err.message);
       });
   };
 
@@ -140,10 +136,10 @@ export default class crawl_progress_page extends PureComponent {
     this.props.$fetch
       .get(API.USER_IMPORT)
       .then((res) => {
-        if(res.data > 3){
+        if(res.data > 2){
           this.props.history.replace('/home/crawl_fail_page')
         } else {
-          this.props.history.reload()
+          location.reload()
         }
       }).catch(err=>{
     })
