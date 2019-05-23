a7457825551c613ac87e250f0232a71b883ee768
孙建坤 <sun_jk@suixingpay.com>
2019-05-23 18:43:44 +0800

    修改跳转逻辑

--- src/pages/home/home_page/index.js
+++ src/pages/home/home_page/index.js
@@ -497,7 +497,11 @@ export default class home_page extends PureComponent {
 						return ele.code === bankCode;
 					});
 					const jumpUrl = seleBank && seleBank.length && seleBank[0].href;
-					window.location.href = jumpUrl + `&showTitleBar=NO&agreementEntryText=《个人信息授权书》&agreementUrl=${encodeURIComponent(`${linkConf.BASE_URL}/disting/#/internet_bank_auth_page`)}`;
+					if (jumpUrl) { // 如果银行code一致跳登录页，否则跳列表页
+						window.location.href = jumpUrl + `&showTitleBar=NO&agreementEntryText=《个人信息授权书》&agreementUrl=${encodeURIComponent(`${linkConf.BASE_URL}/disting/#/internet_bank_auth_page`)}`;
+					} else {
+						this.goToNewMoXie();
+					}
 				} else {
 					this.props.toast.info('系统开小差，请稍后重试');
 				}
