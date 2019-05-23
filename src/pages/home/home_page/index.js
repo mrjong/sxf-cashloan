fb1920d9c202c5ab6f5dd830c8f863e662f06c6b
孙建坤 <sun_jk@suixingpay.com>
2019-05-23 14:43:01 +0800

    修改首页逻辑

--- src/pages/home/home_page/index.js
+++ src/pages/home/home_page/index.js
@@ -36,7 +36,6 @@ const API = {
 	CHECK_CARD_AUTH: '/auth/checkCardAuth/', // 查询爬取进度
 	mxoieCardList: '/moxie/mxoieCardList/C', // 魔蝎银行卡列表
 	cashShowSwitch: '/my/switchFlag/cashShowSwitchFlag', // 是否渲染现金分期
-	QUERY_REPAY_INFO: '/bill/queryRepayInfo', // 确认代还信息查询接口
 };
 let token = '';
 let tokenFromStorage = '';
@@ -699,7 +698,14 @@ export default class home_page extends PureComponent {
 							this.getPercent();
 						}
 						if (result.data.indexSts === 'LN0006' || result.data.indexSts === 'LN0008') {
-							this.queryProInfo();
+							let maxAmtArr = []
+							maxAmtArr = result.data && result.data.indexData && result.data.indexData.prodList && result.data.indexData.prodList.length &&
+							result.data.indexData.prodList.map((item, index) => {
+								return item.maxAmt;
+							});
+							this.setState({
+								userMaxAmt: maxAmtArr.length ? Math.max(...maxAmtArr) : ''
+							})
 						}
 					}
 				);
@@ -1237,28 +1243,6 @@ export default class home_page extends PureComponent {
 		}
 		return componentsAddCards;
 	}
-
-	// 获取代还期限列表 还款日期列表
-	queryProInfo = () => {
-		let maxAmtArr = [];
-		const { usrIndexInfo } = this.state;
-		if (usrIndexInfo && usrIndexInfo.indexData && usrIndexInfo.indexData.autId) {
-			this.props.$fetch.get(`${API.QUERY_REPAY_INFO}/${usrIndexInfo.indexData.autId}`).then((result) => {
-				if (result && result.msgCode === 'PTM0000') {
-					if (result.data && result.data.prdList && result.data.prdList.length) {
-						maxAmtArr = result.data.prdList.map((item, index) => {
-							return item.maxAmt;
-						});
-						this.setState({
-							userMaxAmt: Math.max(...maxAmtArr)
-						})
-					}
-				} else {
-					this.props.toast.info(result.msgInfo);
-				}
-			});
-		}
-	};
 
 	render() {
 		const {
