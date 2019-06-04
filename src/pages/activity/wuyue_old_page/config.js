a2f733eeb2c30cb6a6f70656516bbb838898359e
孙建坤 <sun_jk@suixingpay.com>
2019-06-04 14:24:59 +0800

    变量引入活动id

--- src/pages/activity/wuyue_old_page/config.js
+++ src/pages/activity/wuyue_old_page/config.js
@@ -1,6 +1,7 @@
 // 配置活动id等公共变量
+const { PROJECT_ENV } = process.env;
 const activeConfig = {
-    activeId: 'HDAC2019053119050800002', // 活动uuid
+    activeId: PROJECT_ENV === 'pro' || PROJECT_ENV === 'rc' ? 'MMS00107220190604114342204927560' : 'MMS00107720190423133905084228254', // 活动uuid
 };
 
 export default activeConfig;
