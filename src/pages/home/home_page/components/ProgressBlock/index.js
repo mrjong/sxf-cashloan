d8ed4595dfca95fb0b2a796480820cca2aaff105
孙建坤 <sun_jk@suixingpay.com>
2019-06-12 11:45:58 +0800

    修改样式

--- src/pages/home/home_page/components/ProgressBlock/index.js
+++ src/pages/home/home_page/components/ProgressBlock/index.js
@@ -14,9 +14,23 @@ export default class ProgressBlock extends React.Component {
 		return (
 			<WhiteCard showData={showData} handleClick={handleClick}>
 				<div className={style.circle_box}>
-					<img src={finished_img} className={style.left_ico} alt="finished_img" />
-					<Progress percent={Number(percentData)} position="normal" barStyle={{borderColor: '#397BE6',borderWidth: '0.02rem'}}  style={{backgroundColor: '#C9CDD5',}} />
-					<img src={unfinished_img} className={style.right_ico} alt="unfinished_img" />
+					<div className={style.bar_top}>
+						<span>已提升额度</span>
+						<span>自动提现</span>
+					</div>
+					<div className={style.bar_progress}>
+						<img src={finished_img} className={style.left_ico} alt="finished_img" />
+						<Progress percent={Number(percentData)} position="normal" barStyle={{borderColor: '#397BE6',borderWidth: '0.02rem'}}  style={{backgroundColor: '#C9CDD5',marginLeft: '0.18rem',marginRight: '0.18rem'}} />
+						<img src={unfinished_img} className={style.right_ico} alt="unfinished_img" />
+					</div>
+					<div className={style.bar_bottom}>
+						<span>完善信息</span>
+						<span className={style.unfinished_text}>申请成功</span>
+					</div>
+					<div className={style.percent_text}>
+						<span>{Number(percentData)}<i>%</i></span>
+						已完成
+					</div>
 				</div>
 			</WhiteCard>
 		);
