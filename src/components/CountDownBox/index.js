57cb29191e67c502f8d00a8f36884272e2393b24
shawn2016 <787990963@qq.com>
2019-06-12 13:43:23 +0800

    ====

---
+++ src/components/CountDownBox/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import style from './index.scss';
+
+class CountDownBox extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			item: 3,
+			data: [ 1, 2, 3 ]
+		};
+		this.requestClose = (type) => {
+			this.props.onRequestClose(type);
+			if (this.props.autoClose) clearTimeout(this.autoClose);
+		};
+	}
+	getDom = (item) => {
+		if (item >= 0) {
+			setTimeout(() => {
+				this.setState({
+					item: item - 1
+				});
+			}, 1400);
+			return (
+				<div key={item} className={[ style.count, !item ? style.count2 : '' ].join(' ')}>
+					{item ? item : '开始'}
+				</div>
+			);
+		}
+	};
+	render() {
+		const { item } = this.state;
+		return <div>{item >= 0 ? <div className={style.bg}>{this.getDom(item)}</div> : null}</div>;
+	}
+}
+
+export default CountDownBox;
