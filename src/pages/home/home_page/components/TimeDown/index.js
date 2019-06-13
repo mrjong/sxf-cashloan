29fc0b72666848b4a3ad06915eb981700d0e0f9c
shawn2016 <787990963@qq.com>
2019-06-13 16:51:55 +0800

    ==

--- src/pages/home/home_page/components/TimeDown/index.js
+++ src/pages/home/home_page/components/TimeDown/index.js
@@ -1,19 +1,66 @@
 import React from 'react';
 import style from './index.scss';
 export default class TimeDown extends React.Component {
-	constructor(props) {
-		super(props);
+	constructor() {
+		super();
+		this.state = {
+			count: 0,
+			status: 'stopped'
+		};
+		this.handleStateChange = this.handleStateChange.bind(this);
+		this.handleStart = this.handleStart.bind(this);
+	}
+
+	handleStateChange(nextStatus) {
+		this.setState({
+			status: nextStatus
+		});
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (this.state.status !== prevState.status) {
+			switch (this.state.status) {
+				case 'started':
+					this.handleStart();
+					break;
+				case 'stopped':
+					this.setState({
+						count: 0
+					});
+				case 'paused':
+					clearInterval(this.timer);
+			}
+		}
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timer);
+		this.timer = null;
+	}
+
+	handleStart() {
+		this.timer = setInterval(() => {
+			const newCount = this.state.count + 1;
+			this.setState({
+				count: newCount
+			});
+		}, 1000);
 	}
 	render() {
 		const { percentData, showData, handleClick } = this.props;
 		console.log(percentData);
 		return (
-			<div>
-				<div>
-					<span id="_d">00</span>
-					<span id="_h">00</span>
-					<span id="_m">00</span>
-					<span id="_s">00</span>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-4 col-md-offset-4">
+						<h1 className="text-center">计时器</h1>
+						<Clock count={this.state.count} />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-4 col-md-offset-4">
+						<Controls status={this.state.status} onStatusChange={this.handleStateChange} />
+					</div>
 				</div>
 			</div>
 		);
