5486e66842d0b83234a40819cec6645501fbb786
shawn2016 <787990963@qq.com>
2019-06-17 15:34:47 +0800

    ==

--- src/pages/home/home_page/index.js
+++ src/pages/home/home_page/index.js
@@ -893,42 +893,39 @@ export default class home_page extends PureComponent {
 					}
 				);
 				let ischeckIsEngagedUser = null;
+				let AC20190618_618_RESULT = null;
 				let ischeckEngaged = await checkEngaged({
 					$props: this.props,
 					AcCode: 'AC20190618_618'
 				});
+
 				if (ischeckEngaged.msgCode === 'PTM0000') {
 					ischeckIsEngagedUser = await checkIsEngagedUser({
 						$props: this.props,
 						AcCode: 'AC20190618_618'
 					});
+					AC20190618_618_RESULT = await checkIsEngagedUser({
+						$props: this.props,
+						AcCode: 'AC20190618_618_RESULT'
+					});
 				}
-				// (result.data.indexSts === 'LN0001' ||
-				// 		result.data.indexSts === 'LN0002' ||
-				// 		result.data.indexSts === 'LN0010') &&
-				// 	(ischeckEngaged.msgCode === 'PTM0000' &&
-				// 		((ischeckIsEngagedUser.data &&
-				// 			ischeckIsEngagedUser.data.isEngagedUser === '1' &&
-				// 			result.data.indexSts === 'LN0003' ||
-				// 			result.data.indexSts === 'LN0006' ||
-				// 			result.data.indexSts === 'LN0008' ||
-				// 			(ischeckIsEngagedUser.data &&
-				// 				ischeckIsEngagedUser.data.isEngagedUser === '0' &&
-				// 				ischeckIsEngagedUser.data.joinActivityTm <= 15 * 60)))
-				// )
+
 				if (
-					(ischeckEngaged &&
+					(AC20190618_618_RESULT &&
+						AC20190618_618_RESULT.data &&
+						AC20190618_618_RESULT.data.isEngagedUser === '1' &&
+						ischeckEngaged &&
 						ischeckEngaged.msgCode === 'PTM0000' &&
-						ischeckIsEngagedUser &&
-						ischeckIsEngagedUser.data &&
-						ischeckIsEngagedUser.data.joinActivityTm <= 15 * 60 &&
 						(ischeckIsEngagedUser.data.isEngagedUser === '1' &&
 							(result.data.indexSts === 'LN0001' ||
 								result.data.indexSts === 'LN0002' ||
 								result.data.indexSts === 'LN0004' ||
 								result.data.indexSts === 'LN0003' ||
 								result.data.indexSts === 'LN0010'))) ||
-					(ischeckIsEngagedUser.data.isEngagedUser === '0' &&
+					(ischeckIsEngagedUser &&
+						ischeckIsEngagedUser.data &&
+						ischeckIsEngagedUser.data.joinActivityTm <= 15 * 60 &&
+						ischeckIsEngagedUser.data.isEngagedUser === '0' &&
 						(result.data.indexSts === 'LN0006' || result.data.indexSts === 'LN0008'))
 				) {
 					this.getAC618(ischeckEngaged, ischeckIsEngagedUser);
