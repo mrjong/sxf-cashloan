e57f35f23ba6ad721bc27d015835596cf0edfa5e
shawn2016 <787990963@qq.com>
2019-06-17 11:09:07 +0800

    ==

--- src/pages/home/home_page/index.js
+++ src/pages/home/home_page/index.js
@@ -906,6 +906,7 @@ export default class home_page extends PureComponent {
 				if (
 					(result.data.indexSts === 'LN0001' ||
 						result.data.indexSts === 'LN0002' ||
+						result.data.indexSts === 'LN0003' ||
 						result.data.indexSts === 'LN0010') &&
 					(ischeckEngaged.msgCode === 'PTM0000' &&
 						((ischeckIsEngagedUser.data && ischeckIsEngagedUser.data.isEngagedUser === '1') ||
@@ -915,9 +916,7 @@ export default class home_page extends PureComponent {
 				) {
 					this.getAC618(ischeckEngaged, ischeckIsEngagedUser);
 				} else if (
-					(result.data.indexSts === 'LN0003' ||
-						result.data.indexSts === 'LN0006' ||
-						result.data.indexSts === 'LN0008') &&
+					(result.data.indexSts === 'LN0006' || result.data.indexSts === 'LN0008') &&
 					!store.getShowActivityModal() &&
 					(ischeckEngaged.msgCode !== 'PTM0000' ||
 						(ischeckIsEngagedUser.data && ischeckIsEngagedUser.data.isEngagedUser === '1') ||
