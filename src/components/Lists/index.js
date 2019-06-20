93eb1269bc7b120dac65074ebf1e12f0c607592b
shawn2016 <787990963@qq.com>
2019-06-20 15:36:39 +0800

    ==

--- src/components/Lists/index.js
+++ src/components/Lists/index.js
@@ -1,59 +1,68 @@
 import React, { PureComponent } from 'react';
 import { List } from 'antd-mobile';
-import ListDesc from '../ListDesc'
+import ListDesc from '../ListDesc';
 import styles from './index.scss';
 
 export default class Lists extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  componentWillMount() {
+	componentWillMount() {}
+	getExtra = (list) => {
+		let extraList = [];
+		list.forEach((item, index) => {
+			extraList.push(
+				<span key={index} style={{ color: item.color, fontSize: '0.34rem' }}>
+					{item.name}
+				</span>
+			);
+		});
+		return extraList;
+	};
 
-  }
-  getExtra = (list) => {
-    let extraList = []
-    list.forEach((item, index) => {
-      extraList.push(<span key={index} style={{ color: item.color, fontSize: '0.34rem' }}>{item.name}</span>)
-    });
-    return extraList
-  }
-
-  render() {
-    const { listsInf, className } = this.props;
-    const Item = List.Item;
-    const Brief = Item.Brief;
-    return (
-      <div className={`${styles.listsContainer} ${className} list_box`}>
-        <List
-        >
-          {
-            listsInf.map((item, index) => {
-              const icon = <i className={item.label.className}></i>
-              return (
-                <div key={index}>
-                  <Item
-                    className={item.label.className ? styles.hasIcon : null}
-                    arrow={item.arrowHide ? item.arrowHide : 'horizontal'}
-                    onClick={() => { this.props.clickCb(item) }}
-                    extra={Object.prototype.toString.call(item.extra) === '[object Array]' ? this.getExtra(item.extra) : <span style={{ color: item.extra && item.extra.color }}>{item.extra && item.extra.name}</span>}
-                    thumb={icon}
-                  >
-                    {item.label.name}
-                    {
-                      item.label.brief ? <Brief>{item.label.brief}</Brief> : null
-                    }
-                  </Item>
-                  {
-                    item.feeInfos && item.showDesc ?
-                      <div><ListDesc isClear={item.isClear} listdescinfo={item.feeInfos}></ListDesc></div> : null
-                  }
-                </div>
-              )
-            })
-          }
-        </List>
-      </div>
-    );
-  }
+	render() {
+		const { listsInf, className } = this.props;
+		const Item = List.Item;
+		const Brief = Item.Brief;
+		return (
+			<div className={`${styles.listsContainer} ${className} list_box`}>
+				<List>
+					{listsInf.map((item, index) => {
+						const icon = <i className={item.label.className} />;
+						return (
+							<div key={index}>
+								<Item
+									className={item.label.className ? styles.hasIcon : null}
+									arrow={item.arrowHide ? item.arrowHide : 'horizontal'}
+									onClick={() => {
+										this.props.clickCb(item);
+									}}
+									extra={
+										Object.prototype.toString.call(item.extra) === '[object Array]' ? (
+											this.getExtra(item.extra)
+										) : (
+											<span style={{ color: item.extra && item.extra.color }}>
+												{item.extra && item.extra.name}
+											</span>
+										)
+									}
+									thumb={icon}
+								>
+									{item.label.name}
+									{item.label.brief ? <Brief>{item.label.brief}</Brief> : null}
+									{item.label.name === '优惠劵' ? <div className={styles.rightIcon}>7折超级优惠券</div> : null}
+								</Item>
+								{item.feeInfos && item.showDesc ? (
+									<div>
+										<ListDesc isClear={item.isClear} listdescinfo={item.feeInfos} />
+									</div>
+								) : null}
+							</div>
+						);
+					})}
+				</List>
+			</div>
+		);
+	}
 }
