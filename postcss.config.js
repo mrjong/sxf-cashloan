da0685f0ea7463e10d7ebd07f6651feb02a7235e
chenjiong <kapp1e@163.com>
2019-06-21 17:10:16 +0800

    ..

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,7 @@ module.exports = {
 			// selectorBlackList: [], // str/reg 指定不转换的选择器，str时包含字段即匹配
 			// replace: true,
 			// mediaQuery: false, //媒体查询内的px是否转换
-			// minPixelValue: 1.00001 //小于指定数值的px不转换 })
+			minPixelValue: 1.00001 //小于指定数值的px不转换 })
 		})
 	]
 };
