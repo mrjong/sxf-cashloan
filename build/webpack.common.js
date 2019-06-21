56dfdfa706d4508724a03a0c5ec4fe6e36557be9
shawn2016 <787990963@qq.com>
2019-06-21 10:29:50 +0800

    ==1

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -20,6 +20,19 @@ module.exports = function(options) {
 				{
 					test: /\.json$/,
 					loader: 'json-loader'
+        },
+        {
+					test: /\.(jsx|js)?$/,
+					loader: 'eslint-loader',
+					enforce: 'pre',
+					include: [ path.resolve(__dirname, 'src') ], // 指定检查的目录
+					options: {
+						fix: true
+					}
+					// options: {
+					// 	// 这里的配置项参数将会被传递到 eslint 的 CLIEngine
+					// 	formatter: require('eslint-friendly-formatter') // 指定错误报告的格式规范
+					// }
 				},
 				{
 					test: /\.(jsx|js)?$/,
