521e239128482aeb0dd2a0bf1d667397ad7e1cdb
孙建坤 <sun_jk@suixingpay.com>
2019-08-08 13:30:26 +0800

    修改接口调用

--- src/pages/mine/fqa_page/index.js
+++ src/pages/mine/fqa_page/index.js
@@ -7,37 +7,46 @@ import ButtonCustom from 'components/ButtonCustom';
 import fetch from 'sx-fetch';
 
 const API = {
-	GETUSERINF: '/my/getRealInfo' // 获取用户信息
+	queryQYOpenId: '/my/queryUsrQYOpenId' // 七鱼用户标识
 };
 
 @fetch.inject()
 export default class fqa_page extends PureComponent {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			QYConfig: null // 七鱼的openId
+		};
+	}
+
+	componentWillMount() {
+		this.props.$fetch.get(API.queryQYOpenId).then((result) => {
+			if (result && result.msgCode === 'PTM0000' && result.data !== null) {
+				this.setState({
+					QYConfig: result.data
+				});
+			} else {
+				this.props.toast.info(result.msgInfo);
+			}
+		});
 	}
 	componentDidMount() {
 		buriedPointEvent(mine.faq);
 	}
 
 	goOnline = () => {
-		// this.props.$fetch.get(API.GETUSERINF).then(
-		// 	(result) => {
-		// 		if (result && result.msgCode === 'PTM0000' && result.data !== null) {
-
-		// 		} else {
-		//       this.props.toast.info(result.msgInfo);
-		//     }
-		// 	});
+		const { QYConfig } = this.state;
 		ysf('config', {
-			uid: '55445454', // 用户Id
+			uid: QYConfig && QYConfig.uid, // 用户Id
 			robotShuntSwitch: 1, // 机器人优先开关
-			groupid: '397748875', // 客服组id 金融在线（IOS）397748874   金融在线（安卓）397743127   金融在线（微信公众号）397748875
-			robotId: '3411295', // 机器人ID
+			groupid: QYConfig && QYConfig.groupid, // 客服组id 金融在线（IOS）397748874   金融在线（安卓）397743127   金融在线（微信公众号）397748875
+			robotId: QYConfig && QYConfig.robotId, // 机器人ID
 			success: function() {
 				// 成功回调
-				location.href = ysf('url');
-				// ysf('open');
+				location.href = ysf('url', { templateId: QYConfig && QYConfig.templateId });
+				// ysf('open', {
+				//   templateId: 10317938
+				// });
 			},
 			error: function() {
 				// 错误回调
