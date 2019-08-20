290caa48671f260543db6f6ed81658c1c9dc5de4
shawn2016 <787990963@qq.com>
2019-08-20 17:32:46 +0800

    ==

--- src/pages/mine/fqa_page/index.js
+++ src/pages/mine/fqa_page/index.js
@@ -35,26 +35,27 @@ export default class fqa_page extends PureComponent {
 	}
 
 	goOnline = () => {
-		const { QYConfig } = this.state;
-		window.ysf('config', {
-			uid: QYConfig && QYConfig.uid, // 用户Id
-			robotShuntSwitch: 1, // 机器人优先开关
-			groupid: QYConfig && QYConfig.groupid, // 客服组id 金融在线（IOS）397748874   金融在线（安卓）397743127   金融在线（微信公众号）397748875
-			robotId: QYConfig && QYConfig.robotId, // 机器人ID
-			data: JSON.stringify([{ key: 'uid', value: QYConfig && QYConfig.uid, label: '用户ID' }]),
-			success: function() {
-				// 成功回调
-				location.href = window.ysf('url', { templateId: QYConfig && QYConfig.templateId });
-				// ysf('open', {
-				//   templateId: 10317938
-				// });
-			},
-			error: function() {
-				// 错误回调
-				console.log('出错了');
-				// handle error
-			}
-		});
+		this.props.history.push('/mine/qiyu_page');
+		// const { QYConfig } = this.state;
+		// window.ysf('config', {
+		// 	uid: QYConfig && QYConfig.uid, // 用户Id
+		// 	robotShuntSwitch: 1, // 机器人优先开关
+		// 	groupid: QYConfig && QYConfig.groupid, // 客服组id 金融在线（IOS）397748874   金融在线（安卓）397743127   金融在线（微信公众号）397748875
+		// 	robotId: QYConfig && QYConfig.robotId, // 机器人ID
+		// 	data: JSON.stringify([{ key: 'uid', value: QYConfig && QYConfig.uid, label: '用户ID' }]),
+		// 	success: function() {
+		// 		// 成功回调
+		// 		location.href = window.ysf('url', { templateId: QYConfig && QYConfig.templateId });
+		// 		// ysf('open', {
+		// 		//   templateId: 10317938
+		// 		// });
+		// 	},
+		// 	error: function() {
+		// 		// 错误回调
+		// 		console.log('出错了');
+		// 		// handle error
+		// 	}
+		// });
 	};
 
 	render() {
---
+++ src/pages/mine/qiyu_page/index.js
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react';
+import { headerIgnore } from 'utils';
+import styles from './index.scss';
+import ButtonCustom from 'components/ButtonCustom';
+import fetch from 'sx-fetch';
+import IframeProtocol from 'components/IframeProtocol';
+
+const API = {
+	queryQYOpenId: '/my/queryUsrQYOpenId' // 七鱼用户标识
+};
+
+@fetch.inject()
+export default class qiyu_page extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			postData: null // 七鱼的openId
+		};
+	}
+
+	componentWillMount() {
+		this.props.$fetch.get(API.queryQYOpenId).then((result) => {
+			if (result && result.msgCode === 'PTM0000' && result.data !== null) {
+				this.setState({
+					postData: result.data
+				});
+			} else {
+				this.props.toast.info(result.msgInfo);
+			}
+		});
+	}
+
+	render() {
+		return <IframeProtocol postData={this.state.postData} name="qiyu_page" />;
+	}
+}
