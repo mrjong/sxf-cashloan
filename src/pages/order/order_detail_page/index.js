5981aad17745fdb8132d0925e8ab2e2c65ec408d
chenjiong <kapp1e@163.com>
2019-08-28 17:19:48 +0800

    ...

--- src/pages/order/order_detail_page/index.js
+++ src/pages/order/order_detail_page/index.js
@@ -1,8 +1,10 @@
 import React, { PureComponent } from 'react';
 import OrderCommonPage from '../order_common_page';
+import fetch from 'sx-fetch-rjl';
 
+@fetch.inject()
 export default class order_detail_page extends PureComponent {
 	render() {
-		return <OrderCommonPage {...this.props} $fetch={this.props.$fetch} />;
+		return <OrderCommonPage {...this.props} />;
 	}
 }
