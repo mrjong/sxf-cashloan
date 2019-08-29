8947267122038ad1c9b4e6c570dba000c87bd712
shawn2016 <787990963@qq.com>
2019-08-29 15:35:10 +0800

    ==

--- src/pages/mine/feedback_save_page/index.js
+++ src/pages/mine/feedback_save_page/index.js
@@ -2,13 +2,12 @@ import React, { PureComponent } from 'react';
 import Cookie from 'js-cookie';
 import { store } from 'utils/store';
 import fetch from 'sx-fetch';
-import { Icon, TextareaItem } from 'antd-mobile';
+import { Icon, TextareaItem, ImagePicker, WingBlank, SegmentedControl } from 'antd-mobile';
 import Lists from 'components/Lists';
 import { buriedPointEvent } from 'utils/analytins';
 import { mine } from 'utils/analytinsType';
 import styles from './index.scss';
 import { setBackGround } from 'utils/background';
-
 const API = {
 	VIPCARD: '/my/queryUsrMemSts', // 查询用户会员卡状态
 	LOGOUT: '/signup/logout', // 用户退出登陆
@@ -16,19 +15,40 @@ const API = {
 	couponRedDot: '/index/couponRedDot', // 优惠券红点
 	couponCount: '/index/couponCount' // 优惠券红点
 };
-
+const data = [
+	{
+		url: 'https://zos.alipayobjects.com/rmsportal/PZUUCKTRIHWiZSY.jpeg',
+		id: '2121'
+	},
+	{
+		url: 'https://zos.alipayobjects.com/rmsportal/hqQWgTXdrlmVVYi.jpeg',
+		id: '2122'
+	}
+];
 @fetch.inject()
 @setBackGround('#fff')
 export default class mine_page extends PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
+			files: [],
 			textareaVal: '我最大的意见就是没意见。'
 		};
 	}
-
+	onChange = (files, type, index) => {
+		console.log(files, type, index);
+		this.setState({
+			files
+		});
+	};
+	onSegChange = (e) => {
+		const index = e.nativeEvent.selectedSegmentIndex;
+		this.setState({
+			multiple: index === 1
+		});
+	};
 	render() {
-		const { textareaVal = '' } = this.state;
+		const { files, textareaVal = '' } = this.state;
 		return (
 			<div className={[styles.mine_page, 'mine_page_global'].join(' ')}>
 				<div className={styles.textTitle}>输入您的反馈意见（最少6个字）</div>
@@ -47,6 +67,16 @@ export default class mine_page extends PureComponent {
 					count={180}
 				/>
 				<div className={[styles.textTitle, styles.topLine].join(' ')}>上传图片能更好的的帮助我们定位问题</div>
+				<div className={styles.ImagePicker}>
+					<ImagePicker
+						files={files}
+						onChange={this.onChange}
+						onImageClick={(index, fs) => console.log(index, fs)}
+						selectable={files.length < 5}
+						multiple={true}
+						accept="image/jpeg,image/jpg,image/png"
+					/>
+				</div>
 			</div>
 		);
 	}
