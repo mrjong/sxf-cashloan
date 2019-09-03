f073751ff0e582fd935cb2df09eb1bf04b278a8a
chenjiong <kapp1e@163.com>
2019-09-03 19:34:00 +0800

    ..

--- src/pages/mine/help_center_page/components/QuestionModal/index.js
+++ src/pages/mine/help_center_page/components/QuestionModal/index.js
@@ -110,17 +110,9 @@ export default class QuestionModal extends PureComponent {
 						}}
 					/>
 				</div>
-				<p className={styles.modalDesc}>{question.answer}</p>
+				<p className={styles.modalDesc} dangerouslySetInnerHTML={{ __html: question.answer }}></p>
 				<div className={styles.desc_icon}>以上内容是否解决了您的问题</div>
 				<div className={styles.zan_wrap}>
-					<div
-						onClick={() => {
-							this.dianzanClick('noresolve');
-						}}
-					>
-						<span className={[styles.nozan_icon, noresolve && styles.nozan_icon_active].join(' ')}></span>
-						<span>未解决</span>
-					</div>
 					<div
 						onClick={() => {
 							this.dianzanClick('resolve');
@@ -128,6 +120,14 @@ export default class QuestionModal extends PureComponent {
 					>
 						<span className={[styles.zan_icon, resolve && styles.zan_icon_active].join(' ')}></span>
 						<span>已解决</span>
+					</div>
+					<div
+						onClick={() => {
+							this.dianzanClick('noresolve');
+						}}
+					>
+						<span className={[styles.nozan_icon, noresolve && styles.nozan_icon_active].join(' ')}></span>
+						<span>未解决</span>
 					</div>
 				</div>
 			</Modal>
