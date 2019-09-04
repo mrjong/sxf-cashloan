13d5ddb23c2a46e14e97bfc71b914b75d0edce52
孙建坤 <sun_jk@suixingpay.com>
2019-09-04 14:22:29 +0800

    修改连点问题

--- src/pages/login/login_common_page/index.js
+++ src/pages/login/login_common_page/index.js
@@ -146,48 +146,62 @@ export default class login_common_page extends PureComponent {
 		}
 	};
 
+	prePressTime = 0;
 	//去登陆按钮
 	goLogin = () => {
 		const osType = getDeviceType();
-		if (!this.state.smsJrnNo) {
-			Toast.info('请先获取短信验证码');
-			return;
-		}
-		if (!this.state.isChecked) {
-			Toast.info('请先勾选协议');
-			return;
-		}
-		this.props.form.validateFields((err, values) => {
-			if (!err) {
-				buriedPointEvent(daicao.loginBtn);
-				let param = {
-					smsJrnNo: this.state.smsJrnNo, // 短信流水号
-					osType, // 操作系统
-					smsCd: values.smsCd,
-					usrCnl: getH5Channel(), // 用户渠道
-					location: store.getPosition() // 定位地址 TODO 从session取
-				};
-				if (!this.state.disabledInput) {
-					param.mblNo = values.phoneValue; // 手机号
-				}
-				this.props.$fetch.post(API.smsForLogin, param).then(
-					(res) => {
-						if (res.msgCode !== 'PTM0000') {
-							res.msgInfo && Toast.info(res.msgInfo);
-							return;
-						}
-						Cookie.set('fin-v-card-token', res.data.tokenId, { expires: 365 });
-						// TODO: 根据设备类型存储token
-						store.setToken(res.data.tokenId);
-						recordContract({
-							contractType: '01,02'
-						});
-						if (!store.getQueryUsrSCOpenId()) {
-							this.props.$fetch.get(API.queryUsrSCOpenId).then((res) => {
-								if (res.msgCode === 'PTM0000') {
-									window.sa.login(res.data);
-									store.setQueryUsrSCOpenId(res.data);
-								}
+		const nowTime = Date.now();
+		if (nowTime - this.prePressTime > 1600 || !this.prePressTime) {
+			this.prePressTime = nowTime;
+			if (!this.state.smsJrnNo) {
+				Toast.info('请先获取短信验证码');
+				return;
+			}
+			if (!this.state.isChecked) {
+				Toast.info('请先勾选协议');
+				return;
+			}
+			this.props.form.validateFields((err, values) => {
+				if (!err) {
+					buriedPointEvent(daicao.loginBtn);
+					let param = {
+						smsJrnNo: this.state.smsJrnNo, // 短信流水号
+						osType, // 操作系统
+						smsCd: values.smsCd,
+						usrCnl: getH5Channel(), // 用户渠道
+						location: store.getPosition() // 定位地址 TODO 从session取
+					};
+					if (!this.state.disabledInput) {
+						param.mblNo = values.phoneValue; // 手机号
+					}
+					this.props.$fetch.post(API.smsForLogin, param).then(
+						(res) => {
+							if (res.msgCode !== 'PTM0000') {
+								res.msgInfo && Toast.info(res.msgInfo);
+								return;
+							}
+							Cookie.set('fin-v-card-token', res.data.tokenId, { expires: 365 });
+							// TODO: 根据设备类型存储token
+							store.setToken(res.data.tokenId);
+							recordContract({
+								contractType: '01,02'
+							});
+							if (!store.getQueryUsrSCOpenId()) {
+								this.props.$fetch.get(API.queryUsrSCOpenId).then((res) => {
+									if (res.msgCode === 'PTM0000') {
+										window.sa.login(res.data);
+										store.setQueryUsrSCOpenId(res.data);
+									}
+									this.setState(
+										{
+											showDownloadModal: true
+										},
+										() => {
+											this.startCountDown();
+										}
+									);
+								});
+							} else {
 								this.setState(
 									{
 										showDownloadModal: true
@@ -196,29 +210,20 @@ export default class login_common_page extends PureComponent {
 										this.startCountDown();
 									}
 								);
-							});
-						} else {
-							this.setState(
-								{
-									showDownloadModal: true
-								},
-								() => {
-									this.startCountDown();
-								}
-							);
+							}
+						},
+						(error) => {
+							error.msgInfo &&
+								Toast.info(error.msgInfo, 3, () => {
+									this.state.disabledInput && this.getImage();
+								});
 						}
-					},
-					(error) => {
-						error.msgInfo &&
-							Toast.info(error.msgInfo, 3, () => {
-								this.state.disabledInput && this.getImage();
-							});
-					}
-				);
-			} else {
-				Toast.info(getFirstError(err));
-			}
-		});
+					);
+				} else {
+					Toast.info(getFirstError(err));
+				}
+			});
+		}
 	};
 
 	// 处理获取验证码按钮点击事件
