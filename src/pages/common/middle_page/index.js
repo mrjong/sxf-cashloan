2128b29494542f268b38399e10aceb48e5eefb0c
chenjiong <kapp1e@163.com>
2019-09-15 10:33:25 +0800

    ..

--- src/pages/common/middle_page/index.js
+++ src/pages/common/middle_page/index.js
@@ -1,20 +1,19 @@
 /*
  * @Author: shawn
- * @LastEditTime: 2019-09-11 19:34:24
+ * @LastEditTime: 2019-09-11 10:49:20
  */
 import React, { Component } from 'react';
 import { store } from 'utils/store';
-// import qs from 'qs';
+import qs from 'qs';
 import fetch from 'sx-fetch';
 import { getNextStr } from 'utils';
 import Blank from 'components/Blank';
 import { buriedPointEvent } from 'utils/analytins';
 import { home } from 'utils/analytinsType';
 
-// const API = {
-// 	getXMURL: '/auth/zmAuth', // 芝麻认证之后的回调状态
-// 	updateCredStsForHandle: '/auth/updateCredStsForHandle'
-// };
+const API = {
+	updateCredStsForHandle: '/auth/updateCredStsForHandle'
+};
 @fetch.inject()
 export default class middle_page extends Component {
 	constructor(props) {
@@ -25,72 +24,104 @@ export default class middle_page extends Component {
 	}
 	componentWillMount() {
 		store.removeGoMoxie();
-		// TODO
-		// this.buryPointsType(taskType, true);
-		store.removeGotoMoxieFlag(); //删除去到第三方魔蝎的标志
-		if (store.getNeedNextUrl() && !store.getToggleMoxieCard()) {
-			getNextStr({
-				$props: this.props
-			});
-		} else {
-			this.goRouter();
-		}
-		// TODO
 		//芝麻信用的回调
-		// const query = qs.parse(this.props.history.location.search, { ignoreQueryPrefix: true });
-		// const { taskType, mxcode } = query;
-		// if (taskType) {
-		// 	this.props.$fetch
-		// 		.get(`${API.updateCredStsForHandle}/${taskType}`)
-		// 		.then((res) => {
-		// 			if (res.msgCode !== 'PTM0000') {
-		// 				this.buryPointsType(taskType, false, res.msgInfo);
-		// 				this.props.toast.info(res.msgInfo);
-		// 				this.setState({
-		// 					errorInf:
-		// 						'加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
-		// 				});
-		// 				return;
-		// 			}
-		// 			this.buryPointsType(taskType, true);
-		// 			store.removeGotoMoxieFlag(); //删除去到第三方魔蝎的标志
-		// 			if (store.getNeedNextUrl() && !store.getToggleMoxieCard()) {
-		// 				getNextStr({
-		// 					$props: this.props
-		// 				});
-		// 			} else {
-		// 				this.goRouter();
-		// 			}
-		// 		})
-		// 		.catch((err) => {
-		// 			err.msgInfo && this.buryPointsType(taskType, false, err.msgInfo);
-		// 			this.setState({
-		// 				errorInf:
-		// 					'加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
-		// 			});
-		// 		});
-		// } else if (mxcode && mxcode === -1) {
-		// 	/** mxcode
-		//          * 当配置了backUrl，自动跳转到该backUrl时，添加该参数到backUrl上
-		//             -4 用户输入出错（密码等输错且未继续输入）
-		//             -3 魔蝎数据服务异常
-		//             -2 平台方服务问题（如中国移动维护等）
-		//             -1 默认状态（用于没有进行操作退出）
-		//             0 认证失败，异常错误
-		//             1 任务进行成功
-		//             2 任务进行中
-		//          */
-		// 	if (store.getNeedNextUrl() && !store.getToggleMoxieCard()) {
-		// 		this.props.history.push('/home/home');
-		// 	} else {
-		// 		this.props.history.back();
-		// 	}
-		// } else {
-		// 	this.setState({
-		// 		errorInf:
-		// 			'加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
-		// 	});
-		// }
+		const query = qs.parse(this.props.history.location.search, { ignoreQueryPrefix: true });
+		const { taskType, mxcode, token, type, medium_type } = query;
+		if (token && medium_type === 'app') {
+			store.setToken(token);
+		}
+
+		if (type === 'mxOperator' || type === 'mxCard') {
+			if (taskType) {
+				this.props.$fetch
+					.get(`${API.updateCredStsForHandle}/${taskType}`)
+					.then((res) => {
+						if (res.msgCode !== 'PTM0000') {
+							this.buryPointsType(taskType, false, res.msgInfo);
+							this.props.toast.info(res.msgInfo);
+							this.setState({
+								errorInf:
+									'加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
+							});
+							return;
+						}
+						this.buryPointsType(taskType, true);
+						store.removeGotoMoxieFlag(); //删除去到第三方魔蝎的标志
+						if (store.getNeedNextUrl() && !store.getToggleMoxieCard()) {
+							getNextStr({
+								$props: this.props
+							});
+						} else {
+							this.goRouter();
+						}
+					})
+					.catch((err) => {
+						err.msgInfo && this.buryPointsType(taskType, false, err.msgInfo);
+						this.setState({
+							errorInf:
+								'加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
+						});
+					});
+			} else if (mxcode && mxcode === -1) {
+				/** mxcode
+               * 当配置了backUrl，自动跳转到该backUrl时，添加该参数到backUrl上
+                  -4 用户输入出错（密码等输错且未继续输入）
+                  -3 魔蝎数据服务异常
+                  -2 平台方服务问题（如中国移动维护等）
+                  -1 默认状态（用于没有进行操作退出）
+                  0 认证失败，异常错误
+                  1 任务进行成功
+                  2 任务进行中
+               */
+				if (store.getNeedNextUrl() && !store.getToggleMoxieCard()) {
+					this.props.history.push('/home/home');
+				} else {
+					this.props.history.back();
+				}
+			} else {
+				this.setState({
+					errorInf:
+						'加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
+				});
+			}
+		} else if (type === 'jfOperator' || type === 'jfCard') {
+			let taskType = type === 'jfOperator' ? 'carrier' : 'bank';
+			this.props.$fetch
+				.get(`${API.updateCredStsForHandle}/${taskType}`)
+				.then((res) => {
+					if (res.msgCode !== 'PTM0000') {
+						this.buryPointsType(taskType, false, res.msgInfo);
+						this.props.toast.info(res.msgInfo);
+						this.setState({
+							errorInf:
+								'加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
+						});
+						return;
+					}
+					if (medium_type === 'app') {
+						setTimeout(() => {
+							window.postMessage(JSON.stringify(query), () => {});
+						}, 0);
+					} else {
+						this.buryPointsType(taskType, true);
+						store.removeGotoMoxieFlag(); //删除去到第三方魔蝎的标志
+						if (store.getNeedNextUrl() && !store.getToggleMoxieCard()) {
+							getNextStr({
+								$props: this.props
+							});
+						} else {
+							this.goRouter();
+						}
+					}
+				})
+				.catch((err) => {
+					err.msgInfo && this.buryPointsType(taskType, false, err.msgInfo);
+					this.setState({
+						errorInf:
+							'加载失败,请点击<a href="javascript:void(0);" onclick="window.location.reload()">重新加载</a>'
+					});
+				});
+		}
 	}
 	goRouter = () => {
 		const moxieBackUrl = store.getMoxieBackUrl();
