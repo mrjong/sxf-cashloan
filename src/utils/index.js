29fddb9b59130aaaed246cb3a33de500156e4dd5
chenjiong <kapp1e@163.com>
2019-09-15 11:00:04 +0800

    ..

--- src/utils/index.js
+++ src/utils/index.js
@@ -126,7 +126,7 @@ export const pagesIgnore = (pathname = window.location.pathname) => {
 			'/home/loan_apply_succ_page', // 因为app直接跳转到h5的webview，所以放开
 			'/mine/qiyu', // 因为app直接跳转到h5的webview，所以放开
 			'/home/loan_person_succ_page', // 因为app直接跳转到h5的webview，所以放开
-			'/common/jf_app_middle_page'
+			'/common/middle_page'
 		];
 		if (isWXOpen()) {
 			let pageListWx = ['/home/home', '/common/wx_middle_page', '/mpos/mpos_ioscontrol_page'];
