f0c68611a63fbeaef1fecf3cd09e30aa1beca4d0
shawn2016 <787990963@qq.com>
2019-10-12 10:38:05 +0800

    ==

--- src/pages/common/postmessage_app/index.js
+++ src/pages/common/postmessage_app/index.js
@@ -1,28 +1,51 @@
 /*
  * @Author: shawn
- * @LastEditTime: 2019-10-11 15:33:22
+ * @LastEditTime: 2019-10-12 10:37:23
  */
 import React, { PureComponent } from 'react';
 import ButtonCustom from 'components/ButtonCustom';
 import styles from './index.scss';
-
+let timer = '';
 export default class postmessage_app extends PureComponent {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			showPage: false
+		};
+	}
+	componentWillMount() {
+		if (!sessionStorage.getItem('showPage')) {
+			sessionStorage.setItem('showPage', true);
+			window.location.reload();
+		}
+	}
+	componentDidMount() {
+		timer = setTimeout(() => {
+			this.setState({
+				showPage: true
+			});
+		}, 5000);
+	}
+	componentWillUnmount() {
+		timer && clearTimeout(timer);
 	}
 	// 重新加载
 	reloadHandler = () => {
 		window.location.reload();
 	};
 	render() {
+		const { showPage } = this.state;
 		return (
-			<div className={styles.err_page}>
-				<i className={styles.err_img}></i>
-				<p className={styles.err_cont}>对不起，您找的页面走丢了～</p>
-				<ButtonCustom onClick={this.reloadHandler} className={styles.reload_btn}>
-					返回首页
-				</ButtonCustom>
+			<div>
+				{showPage ? (
+					<div className={styles.err_page}>
+						<i className={styles.err_img}></i>
+						<p className={styles.err_cont}>对不起，您找的页面走丢了～</p>
+						<ButtonCustom onClick={this.reloadHandler} className={styles.reload_btn}>
+							返回首页
+						</ButtonCustom>
+					</div>
+				) : null}
 			</div>
 		);
 	}
