17e71b3f7cb89051ff283ce5eba2c2cc95b31b27
张晋佩 <302784355@qq.com>
2020-02-03 11:47:20 +0800

    修改任务

---
+++ app/assets/lib/globalConfig.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: shawn
+ * @LastEditTime: 2019-09-03 15:14:49
+ */
+function initConfig() {
+	window.globalConfig = {
+		MDopen: true, // 埋点开关
+		wxTest: true // 微信测试开关
+	};
+}
+initConfig();
