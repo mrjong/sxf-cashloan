2f8ca2e0620de082a7801aa5e81a8c330c77cdb3
张晋佩 <302784355@qq.com>
2020-02-18 13:11:53 +0800

    修改刷新token逻辑

--- src/pages/common/routerPage/index.js
+++ src/pages/common/routerPage/index.js
@@ -3,11 +3,14 @@
  * @LastEditTime : 2020-02-10 15:33:18
  */
 import React, { PureComponent } from 'react';
+import qs from 'qs';
 import Routers from 'pages/router';
 import errPage from 'pages/common/err_page';
 import Header from 'components/Header';
 import fetch from 'sx-fetch';
 import { connect } from 'react-redux';
+import { setUserInfoAction } from 'reduxes/actions/staticActions';
+
 import Footer from 'components/Footer';
 import { Toast } from 'antd-mobile';
 import Cookie from 'js-cookie';
@@ -20,10 +23,15 @@ import { HomeModal } from '../../home/home_page/components';
 
 const { PROJECT_ENV } = process.env;
 @fetch.inject()
-@connect((state) => ({
-	userInfo: state.staticState.userInfo,
-	homeData: state.commonState.homeData
-}))
+@connect(
+	(state) => ({
+		userInfo: state.staticState.userInfo,
+		homeData: state.commonState.homeData
+	}),
+	{
+		setUserInfoAction
+	}
+)
 export default class router_Page extends PureComponent {
 	constructor(props) {
 		super(props);
@@ -50,10 +58,90 @@ export default class router_Page extends PureComponent {
 		console.log('+++++++++++++++++++++++');
 		this.acRouter(this.props);
 	}
-	componentWillReceiveProps(nextProps) {
+	async componentWillReceiveProps(nextProps) {
+		console.log(this.props, nextProps, '000000');
 		this.acRouter(nextProps);
 		store.setHistoryRouter(location.pathname);
+
+		const queryData = qs.parse(this.props.history.location.search, {
+			ignoreQueryPrefix: true
+		});
+
+		if (queryData.tokenId) {
+			if (this.props.userInfo && this.props.userInfo.tokenId) {
+				// if (queryData.tokenId !== this.props.userInfo.tokenId) {
+				// 	const newUserInfo = await this.getUserInfo(true);
+				// 	console.log('00');
+				// 	this.props.setUserInfoAction(newUserInfo);
+				// }
+
+				const isUserInfoEqual = this.compare(this.props.userInfo, nextProps.userInfo);
+				console.log(isUserInfoEqual, 'isUserInfoEqual');
+
+				if (!isUserInfoEqual) {
+					const newUserInfo = await this.getUserInfo(true);
+					console.log('11');
+					this.props.setUserInfoAction(newUserInfo);
+				}
+			} else {
+				const newUserInfo = await this.getUserInfo(true);
+				console.log('22');
+				this.props.setUserInfoAction(newUserInfo);
+			}
+		}
 	}
+
+	getUserInfo(flag) {
+		return new Promise((resolve, reject) => {
+			if (flag) {
+				setTimeout(() => {
+					console.log('flag true');
+					resolve({
+						faceFlag: '1',
+						idCheckFlag: '2',
+						idNoHid: '411522********3630',
+						nameHid: '**佩',
+						qyGroupId: '397748875',
+						qyOpenId: '79f44d9f982f418093721351d7aee23d',
+						qyRobotId: '3411295',
+						qyTemplateId: '10317938',
+						regChannel: 'OTHER',
+						scOpenId: 'ee10d77d5ec248b3bf34ba4606d74872',
+						telNoHid: '182****04621111',
+						tokenId: '9d672c458de8f665de0242ce06747d85'
+					});
+				}, 1000);
+			} else {
+				setTimeout(() => {
+					console.log('flag false');
+					reject();
+				}, 1000);
+			}
+		});
+	}
+
+	compare(origin, target) {
+		if (typeof target !== 'object') {
+			//target不是对象/数组
+			return origin === target; //直接返回全等的比较结果
+		}
+
+		if (typeof origin !== 'object') {
+			//origin不是对象/数组
+			return false; //直接返回false
+		}
+		for (let key of Object.keys(target)) {
+			//遍历target的所有自身属性的key
+			if (!this.compare(origin[key], target[key])) {
+				//递归比较key对应的value，
+				//value不等，则两对象不等，结束循环，退出函数，返回false
+				return false;
+			}
+		}
+		//遍历结束，所有value都深度比较相等，则两对象相等
+		return true;
+	}
+
 	getTip = () => {
 		let arr = [];
 		for (let index = 0; index < Routers.length; index++) {
