a16d06bd9f7c259c4614ed454889e068409c9d42
孙建坤 <sun_jk@suixingpay.com>
2020-02-20 11:00:17 +0800

    修改首页

---
+++ src/pages/home/home_page/components/Introduce/index.js
@@ -0,0 +1,52 @@
+/*
+ * @Author: sunjiankun
+ * @LastEditors: sunjiankun
+ * @LastEditTime: 2020-02-20 10:55:29
+ */
+import React from 'react';
+import style from './index.scss';
+import Images from 'assets/image';
+
+export default class Introduce extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	componentWillMount() {}
+	render() {
+		const introduceArr = [
+			{
+				image: Images.adorn.intro_ico,
+				title: '信息安全',
+				subTitle: '数据保密'
+			},
+			{
+				image: Images.adorn.intro_ico2,
+				title: '持牌机构',
+				subTitle: '放心合规'
+			},
+			{
+				image: Images.adorn.intro_ico3,
+				title: '支持代还',
+				subTitle: '100+信用卡'
+			}
+		];
+		return (
+			<div className={style.introduce_container}>
+				<h3 className={style.intro_tit}>关于还到</h3>
+				<ul className={style.intro_content_wrap}>
+					{introduceArr.map((item, index) => (
+						<li key={index} className={style.intro_content}>
+							<img alt="icon" src={item.image} className={style.icon_style} />
+							<span className={style.title_style}>{item.title}</span>
+							<span className={style.sub_tit_style}>{item.subTitle}</span>
+						</li>
+					))}
+				</ul>
+				<div className={style.intro_desc_wrap}>
+					<img alt="icon" src={Images.adorn.lock_black} className={style.lock_style} />
+					敏感数据加密传输，且资金流转安全可靠
+				</div>
+			</div>
+		);
+	}
+}
