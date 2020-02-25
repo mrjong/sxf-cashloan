753c592fb7d03716c0b5432ffbfb236def2fc27d
shawn2016 <787990963@qq.com>
2020-02-25 18:12:16 +0800

    ==

--- src/assets/lib/globalConfig.js
+++ src/assets/lib/globalConfig.js
@@ -1,11 +1,11 @@
 /*
  * @Author: shawn
- * @LastEditTime: 2019-09-03 15:14:49
+ * @LastEditTime: 2020-02-25 18:11:52
  */
 function initConfig() {
 	window.globalConfig = {
 		MDopen: true, // 埋点开关
-		wxTest: true // 微信测试开关
+		wxTest: false // 微信测试开关
 	};
 }
 initConfig();
