b6894568fa963397cb8838b045731aceadf18505
孙建坤 <sun_jk@suixingpay.com>
2020-02-28 10:11:30 +0800

    修改配置

--- src/config/link.conf.js
+++ src/config/link.conf.js
@@ -1,6 +1,6 @@
 /*
  * @Author: shawn
- * @LastEditTime: 2020-02-27 19:09:33
+ * @LastEditTime: 2020-02-28 10:10:16
  */
 /**
  * 外链url配置
@@ -13,7 +13,7 @@ const envMap = {
 		MD_URL: 'https://fbds-test.vbillbank.com',
 		DC_URL: 'https://ffp-test.vbillbank.com/lender/common/ServiceAuthorization?MPOS_APP', // 开发环境 MPOS_APP与MPOS_CM渠道不同
 		PDF_URL: 'https://lns-wap-test.vbillbank.com/front', // pdf预览的接口url
-		BASE_URL: 'https://lns-api-test.vbillbank.com',
+		BASE_URL: 'https://lns-wap-test.vbillbank.com',
 		MARKET_URL: 'https://fuf-test.vbillbank.com/static/ls/page/home.html?c1=1000022&c2=1000023', //外部代超页
 		APPSTORE_URL: 'https://itunes.apple.com/cn/app/id1439290777?mt=8'
 	},
@@ -21,7 +21,7 @@ const envMap = {
 		MD_URL: 'https://fbds-test.vbillbank.com',
 		DC_URL: 'https://ffp-test.vbillbank.com/lender/common/ServiceAuthorization?MPOS_APP', // 测试环境 MPOS_APP与MPOS_CM渠道不同
 		PDF_URL: 'https://lns-wap-test.vbillbank.com/front', // pdf预览的接口url
-		BASE_URL: 'https://lns-api-test.vbillbank.com',
+		BASE_URL: 'https://lns-wap-test.vbillbank.com',
 		MARKET_URL: 'https://fuf-test.vbillbank.com/static/ls/page/home.html?c1=1000022&c2=1000023', //外部代超页
 		APPSTORE_URL: 'https://itunes.apple.com/cn/app/id1439290777?mt=8'
 	},
@@ -29,7 +29,7 @@ const envMap = {
 		MD_URL: 'https://fbds.vbillbank.com',
 		DC_URL: 'https://ffp.vbillbank.com/lender/common/ServiceAuthorization?MPOS_APP', // 生产地址
 		PDF_URL: 'https://lns-wap.vbillbank.com/front', // pdf预览的接口url
-		BASE_URL: 'https://lns-api.vbillbank.com',
+		BASE_URL: 'https://lns-wap.vbillbank.com',
 		MARKET_URL: 'https://fuf-front.vbillbank.com/static/ls/page/home.html?c1=1000022&c2=1000023', //外部代超页
 		APPSTORE_URL: 'https://itunes.apple.com/cn/app/id1439290777?mt=8'
 	},
@@ -37,7 +37,7 @@ const envMap = {
 		MD_URL: 'https://fbds-test.vbillbank.com',
 		DC_URL: 'https://ffp.vbillbank.com/lender/common/ServiceAuthorization?MPOS_APP', // 生产地址
 		PDF_URL: 'https://lns-wap-rc.vbillbank.com/front', // pdf预览的接口url
-		BASE_URL: 'https://lns-api-rc.vbillbank.com',
+		BASE_URL: 'https://lns-wap-rc.vbillbank.com',
 		MARKET_URL: 'https://fuf-front.vbillbank.com/static/ls/page/home.html?c1=1000022&c2=1000023', //外部代超页
 		APPSTORE_URL: 'https://itunes.apple.com/cn/app/id1439290777?mt=8'
 	}
